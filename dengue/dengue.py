fde2d7d128534b5bb21ad04558c4c64436c06136
sidneymbell <sidneymb@uw.edu>
2016-11-06 14:46:28 -0800

    Functional checkpoint :tada:

--- dengue/dengue.py
+++ dengue/dengue.py
@@ -11,12 +11,53 @@ from Bio import SeqIO
 from Bio.SeqFeature import FeatureLocation
 import numpy as np
 from datetime import datetime
+import os
 
-attribute_nesting = {'geographic location':['region', 'country', 'location'],}
 
 class dengue_process(process):
     def __init__(self, **kwargs):
-        self.sequence_fname = self.input_data_path+'/dengue_virus_%d.fasta'%self.serotype
+        self.serotype = kwargs['serotype']
+        self.lineage = 'dengue_virus_%s'%self.serotype
+        self.input_data_path = '../fauna/data/'+self.lineage ## Should add an assertion here that these directories exist.
+        self.sequence_fname = self.input_data_path+self.lineage
+        self.store_data_path = 'store/'+self.lineage + '_'
+        self.build_data_path = 'build/'+self.lineage + '_'
+
+        self.dengue = process(input_data_path = self.input_data_path,
+                       store_data_path = self.store_data_path,
+                       build_data_path = self.build_data_path,
+                       reference='./dengue/metadata/dengue_virus_%s_outgroup.gb'%self.serotype,
+                       proteins=['C', 'prM', 'E', 'NS1', '2A', '2B',
+                                 'NS3', '4A', '4B', 'NS5'],
+                       method='SLSQP')
+
+        if params.load:
+            self.dengue.load()
+        else:
+            self.fasta_fields = {0:'strain', 2:'accession', 3:'date', 4:'region', 5:'country',
+                            6:'division', 8:'db', 10:'authors', 11:'latitude', 12:'longitude'}
+            self.dengue.load_sequences(fields=self.fasta_fields)
+            self.dengue.seqs.filter(lambda s: s.attributes['date']>=datetime(2012,1,1).date() and
+                                       s.attributes['date']< datetime(2017,1,1).date())
+            self.dengue.seqs.filter(lambda s: len(s.seq)>=2000)
+            self.dropped_strains = []
+            self.dengue.seqs.filter(lambda s: s.id not in self.dropped_strains)
+            self.dengue.seqs.subsample(category = lambda x:(x.attributes['region'],
+                                                     x.attributes['date'].year,
+                                                     x.attributes['date'].month), threshold=1000)
+
+            os.system('echo $MAFFT_BINARIES')
+            os.system('unset MAFFT_BINARIES')
+            self.dengue.align()
+            self.dengue.build_tree()
+
+        self.dengue.clock_filter(n_iqd=3, plot=True)
+        self.dengue.annotate_tree(Tc=0.005, timetree=True, reroot='best')
+        self.dengue.tree.geo_inference('region')
+        self.dengue.tree.geo_inference('country')
+        self.dengue.tree.geo_inference('division')
+        self.dengue.export(controls = attribute_nesting)
+
 
     def load_reference(self, reference_file):
         from Bio import SeqIO
@@ -35,7 +76,6 @@ class dengue_process(process):
             self.proteins = {}
 
 
-
 if __name__=="__main__":
     import argparse
 
@@ -45,41 +85,5 @@ if __name__=="__main__":
     parser.add_argument('--load', action='store_true', help = 'recover from file')
     parser.add_argument('-serotype', type = str, choices=['1', '2', '3', '4'], help = 'which serotype of dengue to build trees for')
     params = parser.parse_args()
-
-    lineage = 'dengue'
-    input_data_path = '../fauna/data/'+lineage ## Should add an assertion here that these directories exist.
-    store_data_path = 'store/'+lineage + '_'
-    build_data_path = 'build/'+lineage + '_'
-
-    dengue = process(input_data_path = input_data_path,
-                   store_data_path = store_data_path,
-                   build_data_path = build_data_path,
-                   reference='./dengue/metadata/dengue_virus_%s_outgroup.gb'%params.serotype,
-                   proteins=['C', 'prM', 'E', 'NS1', '2A', '2B',
-                             'NS3', '4A', '4B', 'NS5'],
-                   method='SLSQP')
-
-    if params.load:
-        dengue.load()
-    else:
-        fasta_fields = {0:'strain', 2:'accession', 3:'date', 4:'region', 5:'country',
-                        6:'division', 8:'db', 10:'authors', 11:'latitude', 12:'longitude'}
-        dengue.load_sequences(fields=fasta_fields)
-        dengue.seqs.filter(lambda s: s.attributes['date']>=datetime(2012,1,1).date() and
-                                   s.attributes['date']< datetime(2017,1,1).date())
-        dengue.seqs.filter(lambda s: len(s.seq)>=2000)
-        dropped_strains = []
-        dengue.seqs.filter(lambda s: s.id not in dropped_strains)
-        dengue.seqs.subsample(category = lambda x:(x.attributes['region'],
-                                                 x.attributes['date'].year,
-                                                 x.attributes['date'].month), threshold=1000)
-
-        dengue.align()
-        dengue.build_tree()
-
-    dengue.clock_filter(n_iqd=3, plot=True)
-    dengue.annotate_tree(Tc=0.005, timetree=True, reroot='best')
-    dengue.tree.geo_inference('region')
-    dengue.tree.geo_inference('country')
-    dengue.tree.geo_inference('division')
-    dengue.export(controls = attribute_nesting)
+    attribute_nesting = {'geographic location':['region', 'country', 'location'],}
+    dengue_process(**params.__dict__)
