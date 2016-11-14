a7b4383127ea3187c8f8002584053ea26540a3dc
sidneymbell <sidneymb@uw.edu>
2016-11-14 15:38:51 -0800

    Revert to default outgroup parsing.

--- dengue/dengue.py
+++ dengue/dengue.py
@@ -33,14 +33,13 @@ class dengue_process(process):
         self.store_data_path = 'store/'+self.lineage + '_'
         self.build_data_path = 'build/'+self.lineage + '_'
         self.proteins = ['C', 'M', 'E', 'NS1', 'NS2A', 'NS2B', 'NS3', 'NS4A', '2K', 'NS4B', 'NS5']
-        self.load_reference(self.reference_fname, self.proteins)
 
         self.dengue = process(input_data_path = self.input_data_path,
                        store_data_path = self.store_data_path,
                        build_data_path = self.build_data_path,
-                       proteins=self.proteins,
-                       method='SLSQP',
-                       reference_seq = self.reference_seq)
+                       protein_list=self.proteins,
+                       reference=self.reference_fname,
+                       method='SLSQP')
 
         if params.load:
             self.dengue.load()
@@ -49,16 +48,14 @@ class dengue_process(process):
                             5:'division', 6: 'location'}
             self.dengue.load_sequences(fields=self.fasta_fields)
 
-
             self.dengue.seqs.filter(lambda s: s.attributes['date']>=datetime(2012,1,1).date() and
-                                       s.attributes['date']< datetime(2017,1,1).date())
+                                       s.attributes['date']< datetime(2017,1,1).date(), leave_ref=True)
             self.dengue.seqs.filter(lambda s: len(s.seq)>=2000)
             self.dropped_strains = []
             self.dengue.seqs.filter(lambda s: s.id not in self.dropped_strains)
             self.dengue.seqs.subsample(category = lambda x:(x.attributes['region'],
                                                      x.attributes['date'].year,
                                                      x.attributes['date'].month), threshold=1000)
-
             self.dengue.align()
             self.dengue.build_tree()
 
@@ -68,27 +65,6 @@ class dengue_process(process):
         self.dengue.tree.geo_inference('country')
         self.dengue.tree.geo_inference('division')
         self.dengue.export(controls = attribute_nesting)
-
-
-    def load_reference(self, reference_file, protein_list):
-        from Bio import SeqIO
-        from Bio.SeqFeature import FeatureLocation
-        refseq_names = {'1': 'DENV1/NAURUISLAND/REFERENCE/1997', '2': 'DENV2/THAILAND/REFERENCE/1964', '3': 'DENV3/SRI_LANKA/REFERENCE/2000', '4': 'DENV4/NA/REFERENCE/2003'}
-        self.reference_seq = SeqIO.read(reference_file, 'genbank')
-        self.reference_seq.id = refseq_names[self.serotype]
-        self.reference_seq.name = refseq_names[self.serotype]
-        self.genome_annotation = self.reference_seq.features
-        if protein_list != []:
-            # grab annotation from genbank
-            self.proteins = {}
-            for f in self.genome_annotation:
-                if 'product' in f.qualifiers:
-                    mat_peptide = f.qualifiers['product'][0].split()[-1]
-                    location = FeatureLocation(start=f.location.start, end=f.location.end, strand=1)
-                    if mat_peptide in protein_list and mat_peptide not in self.proteins: # keep the larger entry (e.g., keep prM instead of just pr and just M)
-                        self.proteins[mat_peptide] = location
-        else:
-            self.proteins = {}
 
 if __name__=="__main__":
     import argparse
