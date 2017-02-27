2917f5ec7697f415df0b94c9d5700b18c2708df1
Trevor Bedford <trevor@bedford.io>
2017-02-26 23:48:41 -0500

    Update Ebola pipeline with dropped strains and urls

--- ebola/ebola.py
+++ ebola/ebola.py
@@ -124,10 +124,14 @@ if __name__=="__main__":
                    method='SLSQP')
 
     if params.load==False:
-        fasta_fields = {0:'strain', 2:'accession', 3:'date', 4:'region', 5:'country', 6:'division', 8:'db', 10:'authors'}
+        fasta_fields = {0:'strain', 2:'accession', 3:'date', 4:'region', 5:'country', 6:'division', 8:'db', 10:'authors', 11:'url'}
         ebola.load_sequences(fields=fasta_fields)
         ebola.seqs.filter(lambda s: s.attributes['date']>=datetime(2012,1,1).date() and
                                    s.attributes['date']< datetime(2017,1,1).date())
+        dropped_strains = [
+            "EM_074335", "EM_074462" # lack country metadata
+        ]
+        ebola.seqs.filter(lambda s: s.id not in dropped_strains)
         forced_strains = [
             "EM_076610" # flare-up index case
         ]
