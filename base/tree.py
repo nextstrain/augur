02f3603a0b603e906371584f36dbaa75aa783e36
Trevor Bedford <trevor@bedford.io>
2017-03-30 12:01:09 -0700

    Fix merge of tree.py

--- base/tree.py
+++ base/tree.py
@@ -206,42 +206,23 @@ class tree(object):
         self.is_timetree=True
 
 
-    def geo_inference(self, attr, missing='?', root_state=None):
+    def geo_inference(self, attr, root_state=None):
         '''
         infer a "mugration" model by pretending each region corresponds to a sequence
         state and repurposing the GTR inference and ancestral reconstruction
         '''
-        from treetime_augur import GTR
-        # Determine alphabet
+        from treetime import GTR
+        # Determine alphabet and store reconstructed ancestral sequences
         places = set()
-        for node in self.tree.find_clades():
-            if hasattr(node, 'attr'):
-                if attr in node.attr and attr!=missing:
-                    places.add(node.attr[attr])
-
-        # construct GTR (flat for now). The missing DATA symbol is a '-' (ord('-')==45)
-        places = sorted(places)
-        nc = len(places)
-        if nc>180:
-            self.logger("geo_inference: can't have more than 180 places!",1)
-            return
-        elif nc==1:
-            self.logger("geo_inference: only one place found -- setting every internal node to %s!"%places[0],1)
-            for node in self.tree.find_clades():
-                node.attr[attr] = places[0]
-                node.__setattr__(attr+'_transitions',[])
-            return
-        elif nc==0:
-            self.logger("geo_inference: list of places is empty!",1)
-            return
-
-        # store previously reconstructed sequences
         nuc_seqs = {}
         nuc_muts = {}
         nuc_seq_LH = None
         if hasattr(self.tt.tree,'sequence_LH'):
             nuc_seq_LH = self.tt.tree.sequence_LH
         for node in self.tree.find_clades():
+            if hasattr(node, 'attr'):
+                if attr in node.attr:
+                    places.add(node.attr[attr])
             if hasattr(node, 'sequence'):
                 nuc_seqs[node] = node.sequence
             if hasattr(node, 'mutations'):
@@ -250,17 +231,19 @@ class tree(object):
         if root_state is not None:
             places.add(root_state)
 
+        # construct GTR (flat for now). The missing DATA symbol is a '-' (ord('-')==45)
+        places = sorted(places)
+        nc = len(places)
+        if nc<2 or nc>180:
+            self.logger("geo_inference: can't have less than 2 or more than 180 places!",1)
+            return
 
         alphabet = {chr(65+i):place for i,place in enumerate(places)}
+        alphabet_rev = {v:k for k,v in alphabet.iteritems()}
         sequence_gtr = self.tt.gtr
         myGeoGTR = GTR.custom(pi = np.ones(nc, dtype=float)/nc, W=np.ones((nc,nc)),
                               alphabet = np.array(sorted(alphabet.keys())))
-        # ammend the profile map with the character for missing data
-        if missing:
-            missing_char = chr(65+nc)
-            alphabet[missing_char]=missing
-            myGeoGTR.profile_map[missing_char] = np.ones(nc)
-        alphabet_rev = {v:k for k,v in alphabet.iteritems()}
+        myGeoGTR.profile_map['-'] = np.ones(nc)
 
         # set geo info to nodes as one letter sequence.
         for node in self.tree.get_terminals():
@@ -268,7 +251,7 @@ class tree(object):
                 if attr in node.attr:
                     node.sequence=np.array([alphabet_rev[node.attr[attr]]])
             else:
-                node.sequence=np.array([missing_char])
+                node.sequence=np.array(['-'])
         for node in self.tree.get_nonterminals():
             node.__delattr__('sequence')
         if root_state is not None:
@@ -304,6 +287,7 @@ class tree(object):
                     node.mutations = nuc_muts[node]
 
         self.tt.use_mutation_length=tmp_use_mutation_length
+
 
     def get_attr_list(self, get_attr):
         states = []
