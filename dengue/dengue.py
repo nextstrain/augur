51f3bd0acb470274632e6126ae2ed4a41489a6d5
sidneymbell <sidneymb@uw.edu>
2017-04-28 14:34:12 -0700

    Fix region color map.

--- dengue/dengue.py
+++ dengue/dengue.py
@@ -11,18 +11,9 @@ from glob import glob
 
 
 #### Provide metadata ####
-region_cmap = [
-    ["europe",             "#4580CA"],
-    ["china",              "#3F63CF"],
-    ["japan_korea",        "#4272CE"],
-    ["southeast_asia",     "#571EA2"],
-    ["oceania",            "#4B26B1"],
-    ["south_pacific",      "#4433BE"],
-    ["south_america",      "#56A0AE"],
-    ["central_america",    "#BDBB48"],
-    ["caribbean",          "#E67C32"],
-    ["north_america",      "#DC2F24"]
-]
+regions = ["japan_korea","china","south_asia","southeast_asia", "south_pacific","oceania","subsaharan_africa","west_asia","caribbean","south_america","central_america","north_america","europe"]
+colors =  ["#4B26B1", "#3F4ACA", "#4272CE", "#4D92BF", "#5DA8A3", "#74B583", "#8EBC66", "#ACBD51", "#C8B944", "#DDA93C", "#E68B35", "#E3602D", "#DC2F24"],
+region_cmap = zip(regions, colors)
 
 color_options = {
     "region":{"key":"region", "legendTitle":"Region", "menuItem":"region", "type":"discrete", "color_map": region_cmap},
