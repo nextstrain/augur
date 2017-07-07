47a7885d602bd0fc00d00dfba907e48edfa01e49
James Hadfield <jh22@sanger.ac.uk>
2017-07-06 17:35:10 -0700

    mumps README + outbreaks

--- mumps/mumps.prepare.py
+++ mumps/mumps.prepare.py
@@ -8,48 +8,57 @@ import argparse
 
 def collect_args():
     parser = argparse.ArgumentParser(description = "Prepare fauna FASTA for analysis")
-    parser.add_argument('-v', '--viruses_per_month', type = int, default = 15, help='Subsample x viruses per country per month. Set to 0 to disable subsampling. (default: 15)')
     return parser.parse_args()
 
 dropped_strains = []
 
-config = {
-    "dir": "mumps",
-    "file_prefix": "mumps",
-    "input_paths": ["../../fauna/data/mumps.fasta"],
-    "header_fields": {0:'strain', 2:'accession', 3:'date', 4:'region', 5:'country',
-                    6:'division', 8:'db', 10:'authors', 11:'url'},
-    "filters": (
-        ("Dropped Strains", lambda s: s.id not in [fix_names(x) for x in dropped_strains]),
-    ),
-    "subsample": {
-        "category": lambda x:(x.attributes['date'].year, x.attributes['date'].month),
-    },
-    "colors": ["country", "region"],
-    "color_defs": ["./colors.tsv"],
-    "lat_longs": ["country", "region"],
-    "reference": {
-        "path": "mumps-reference.gb",
-        "metadata": {
-            'strain': "MuV/Gabon/13/2", "accession": "KM597072.1", "date": "2013-03-01",
-            'host': "human", 'country': "Gabon"
-        },
-        "include": 0,
-        "genes": ['NC', 'P', 'V', 'I', 'M', 'F', 'SH', 'HN', 'L']
-    }
+filters = {
+    "dropped_strains": ("Dropped Strains", lambda s: s.id not in [fix_names(x) for x in dropped_strains]),
+    "canada_only": ("Canada only", lambda s: s.attributes['country'] == "canada"),
+    "exclude_BC": ("Exclude BC outbreak", lambda s: not s.attributes['accession'].startswith("BC_outbreak")),
 }
+
+def make_config(context):
+    config = {
+        "dir": "mumps",
+        "file_prefix": "mumps_%s"%context,
+        "input_paths": ["../../fauna/data/mumps.fasta"],
+        "header_fields": {0:'strain', 2:'accession', 3:'date', 4:'region', 5:'country',
+                        6:'division', 8:'db', 10:'authors', 11:'url'},
+        "subsample": False,
+        "colors": ["country", "region"],
+        "color_defs": ["./colors.tsv"],
+        "lat_longs": ["country", "region"],
+        "reference": {
+            "path": "mumps-reference.gb",
+            "metadata": {
+                'strain': "MuV/Gabon/13/2", "accession": "KM597072.1", "date": "2013-03-01",
+                'host': "human", 'country': "Gabon"
+            },
+            "include": 0,
+            "genes": ['NC', 'P', 'V', 'I', 'M', 'F', 'SH', 'HN', 'L']
+        }
+    }
+    if context == "global":
+        config["filters"] = (filters["dropped_strains"], filters["exclude_BC"])
+    elif context == "bc":
+        config["filters"] = (filters["dropped_strains"], filters["canada_only"])
+    else:
+        print("Unknown context. FATAL")
+        sys.exit(2)
+
+
+
+    return config
 
 if __name__=="__main__":
     params = collect_args()
-    if params.viruses_per_month == 0:
-        config["subsample"] = False
-    else:
-        config["subsample"]["threshold"] = params.viruses_per_month
-    runner = prepare(config)
-    runner.load_references()
-    runner.applyFilters()
-    runner.ensure_all_segments()
-    runner.subsample()
-    runner.colors()
-    runner.latlongs()
-    runner.write_to_json()
+    for context in ["global", "bc"]:
+        runner = prepare(make_config(context))
+        runner.load_references()
+        runner.applyFilters()
+        runner.ensure_all_segments()
+        runner.subsample()
+        runner.colors()
+        runner.latlongs()
+        runner.write_to_json()
--- mumps/mumps.process.py
+++ mumps/mumps.process.py
@@ -3,34 +3,44 @@ import os, sys
 sys.path.append('..') # we assume (and assert) that this script is running from the virus directory, i.e. inside H7N9 or zika
 from base.process import process
 import argparse
+import glob
 
 parser = argparse.ArgumentParser(description = "Process a prepared zika JSON")
 parser.add_argument('--clean', action='store_true', help="clean build (remove previous checkpoints)")
+parser.add_argument('-j', '--jsons', default=["all"], nargs='+', type=str, help="prepared JSON(s). \"all\" will do them all. Default = all")
 
-config = {
-    "dir": "mumps",
-    "in": "prepared/mumps.json",
-    "newick_tree_options": {"nthreads": 4},
-    "clock_filter": {
-        "n_iqd": 4,
-    },
-    "geo_inference": ["country", "region"],
-    "auspice": { ## settings for auspice JSON export
-        "color_options": {
-            "country":{"key":"country", "legendTitle":"Country", "menuItem":"country", "type":"discrete"},
-            "region":{"key":"region", "legendTitle":"Region", "menuItem":"region", "type":"discrete"},
+def make_config(prepared_json, clean):
+    # Augur currently broken for geo inference if only one trait (i.e. all canada)
+    geo = ["country", "region"]
+    if "bc" in prepared_json:
+        geo = False
+    return {
+        "dir": "mumps",
+        "in": prepared_json,
+        "newick_tree_options": {"nthreads": 4},
+        "clock_filter": {
+            "n_iqd": 4,
         },
-        "controls": {'authors':['authors']}
+        "geo_inference": geo,
+        "auspice": { ## settings for auspice JSON export
+            "color_options": {
+                "country":{"key":"country", "legendTitle":"Country", "menuItem":"country", "type":"discrete"},
+                "region":{"key":"region", "legendTitle":"Region", "menuItem":"region", "type":"discrete"},
+            },
+            "controls": {'authors':['authors']}
+        },
+        "clean": clean,
     }
-}
 
 if __name__=="__main__":
     params = parser.parse_args()
-    if params.clean: config["clean"] = True
-    runner = process(config)
-    runner.align()
-    runner.build_tree()
-    runner.timetree_setup_filter_run()
-    runner.run_geo_inference()
-    runner.save_as_nexus()
-    runner.auspice_export()
+    jsons = glob.glob("prepared/*.json") if "all" in params.jsons else params.jsons
+    for prepared_json in jsons:
+        print("Processing {}".format(prepared_json))
+        runner = process(make_config(prepared_json, params.clean))
+        runner.align()
+        runner.build_tree()
+        runner.timetree_setup_filter_run()
+        runner.run_geo_inference()
+        runner.save_as_nexus()
+        runner.auspice_export()
