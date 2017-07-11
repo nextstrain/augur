ed3af61c10bccb69ecfa59c21e3de5745c462a80
sidneymbell <sidneymb@uw.edu>
2017-07-11 14:05:07 -0700

    Typo

--- dengue/dengue.process.wrapper.py
+++ dengue/dengue.process.wrapper.py
@@ -6,13 +6,13 @@ running = []
 job_id = 0
 
 while True:
-    running = [r for r in running if r.poll() is None]
-    if len(running) != 2:
-    if job_id == len(cmds):
-        break
+	running = [r for r in running if r.poll() is None]
+	if len(running) != 2:
+		if job_id == len(cmds):
+			break
 
-    while len(running) < 2:
-        proc = subprocess.Popen(cmds[job_id].split())
-        print 'STARTING JOB ID %d'%job_id
-        job_id += 1
-        running.append(proc)
+		while len(running) < 2:
+			proc = subprocess.Popen(cmds[job_id].split())
+			print 'STARTING JOB ID %d'%job_id
+			job_id += 1
+			running.append(proc)
