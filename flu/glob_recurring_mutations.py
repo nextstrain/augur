b34b91ae38ba37bfcbcab6f031332acd704f6eef
Richard Neher <richard.neher@unibas.ch>
2017-09-04 00:01:53 +0200

    added simple scripts to assemble mutation jsons

---
+++ flu/glob_recurring_mutations.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import os, sys, glob, json
+
+lineages = ['H3N2', 'H1N1pdm', 'Vic', 'Yam']
+res = ['2y' ,'3y', '6y']
+passage = ['cell', 'egg']
+assay = ['hi', 'fra']
+
+def return_struct():
+    return {k:{k2:{k3:[] for k3 in assay} for k2 in passage} for k in res}
+
+recurring_positions = {'H3N2':return_struct(), 'H1N1pdm':return_struct(),
+                    'Vic':return_struct(), 'Yam':return_struct()}
+
+recurring_mutations = {'H3N2':return_struct(), 'H1N1pdm':return_struct(),
+                    'Vic':return_struct(), 'Yam':return_struct()}
+
+
+for l in lineages:
+    for r in res:
+        for p in passage:
+            for a in assay:
+                try:
+                    fname = 'recurring_mutations/flu_%s_ha_%s_%s_%s_recurring_positions.txt'%(l.lower(), r, p, a)
+                    with open(fname, 'r') as ifile:
+                        tmp =[]
+                        for line in ifile:
+                            if line[0]=='#' and len(line.strip()):
+                                continue
+                            tmp.append(line.strip().split())
+                    recurring_positions[l][r][p][a] = tmp
+
+                    fname = 'recurring_mutations/flu_%s_ha_%s_%s_%s_recurring_mutations.txt'%(l.lower(), r, p, a)
+                    with open(fname, 'r') as ifile:
+                        tmp =[]
+                        for line in ifile:
+                            if line[0]=='#' and len(line.strip()):
+                                continue
+                            tmp.append(line.strip().split())
+                    recurring_mutations[l][r][p][a] = tmp
+                except:
+                    pass
+
+with open('recurring_mutations.json', 'w') as ofile:
+    json.dump(recurring_mutations, ofile)
+
+with open('recurring_positions.json', 'w') as ofile:
+    json.dump(recurring_positions, ofile)
+
