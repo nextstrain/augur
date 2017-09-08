3c81852406ca90652988316ff80bdf7f3bba4dda
Richard Neher <richard.neher@unibas.ch>
2017-09-08 23:51:30 +0200

    only do mutation frequencies when necessary

--- flu/run_flu.py
+++ flu/run_flu.py
@@ -10,10 +10,14 @@ for lineage in ['h3n2', 'h1n1pdm', 'vic', 'yam']:
 					continue
 
 				call = ['python', 'flu.prepare.py', '-r', resolution,  '-l', lineage, '--titers', '../../fauna/data/%s_cdc_%s_%s_titers.tsv'%(lineage, titer, passage),
-					'--sequences', '../../fauna/data/%s.fasta'%lineage, '--file_prefix', 'flu_%s_ha_%s_%s_%s'%(lineage, resolution, passage, titer), '--complete_frequencies']
+					'--sequences', '../../fauna/data/%s.fasta'%lineage, '--file_prefix', 'flu_%s_ha_%s_%s_%s'%(lineage, resolution, passage, titer)]
+				if passage=='cell' and (titer=='hi' or lineage!='h3n2'):
+					call.append('--complete_frequencies')
 
 				os.system(' '.join(call))
 				call = ['qsub', 'submit_script.sh', 'flu.process.py', '-j', 'prepared/flu_%s_ha_%s_%s_%s.json'%(lineage, resolution, passage, titer)]
+				if not (passage=='cell' and (titer=='hi' or lineage!='h3n2')):
+					call.append('--no_mut_freqs')
 
 				os.system(' '.join(call))
 				print(' '.join(call))
