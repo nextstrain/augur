f34f1dc5df940d9bb55c2957300d69bf590808ae
Trevor Bedford <trevor@bedford.io>
2017-09-09 23:11:59 -0700

    Update flu run script for slurm

--- flu/run_flu.py
+++ flu/run_flu.py
@@ -19,8 +19,11 @@ def build_live(system="local", frequencies="complete"):
             call = [
                 'flu.process.py',
                 '--json', 'prepared/flu_%s_ha_%s.json'%(lineage, resolution)]
-            if (system == "cluster"):
+            if (system == "qsub"):
                 call = ['qsub', 'submit_script.sh'] + call
+            elif (system == "sbatch"):
+                concat = '"' + ' '.join(call) + '"'
+                call = ['sbatch', '-n', '1', '-c', '2', '--mem', '8096', '--time', '12:00:00', '--wrap', concat]
             elif (system == "local"):
                 call = ['python'] + call
             print(' '.join(call))
@@ -50,8 +53,11 @@ def build_cdc(system="local", frequencies="complete"):
                         'flu.process.py',
                         '--json', 'prepared/flu_%s_ha_%s_%s_%s.json'%(lineage, resolution, passage, assay),
                         '--titers_export']
-                    if (system == "cluster"):
+                    if (system == "qsub"):
                         call = ['qsub', 'submit_script.sh'] + call
+                    elif (system == "sbatch"):
+                        concat = '"' + ' '.join(call) + '"'
+                        call = ['sbatch', '-n', '1', '-c', '2', '--mem', '8096', '--time', '12:00:00', '--wrap', concat]
                     elif (system == "local"):
                         call = ['python'] + call
                     print(' '.join(call))
@@ -62,7 +68,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Run flu builds')
     parser.add_argument('-b', '--build', type = str, default = 'live', help='build to run, live or cdc')
-    parser.add_argument('-s', '--system', type = str, default = 'local', help='where to run, local or cluster')
+    parser.add_argument('-s', '--system', type = str, default = 'local', help='where to run, local, qsub or sbatch')
     parser.add_argument('--frequencies', type = str, default = 'complete', help='frequencies to complete, complete or subsampled')
     params = parser.parse_args()
 
