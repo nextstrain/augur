67b9f10f399726074ed454a717575d32339fe549
John Huddleston <huddlej@gmail.com>
2017-09-12 15:37:21 -0700

    Reverted to annotating fitness as a node attribute without assuming an 'attr' attribute exists.

--- base/fitness_model.py
+++ base/fitness_model.py
@@ -462,9 +462,9 @@ class fitness_model(object):
 
         for node in self.nodes:
             if node.predictors[final_timepoint] is not None:
-                node.attr["fitness"] = self.fitness(self.model_params, node.predictors[final_timepoint])
+                node.fitness = self.fitness(self.model_params, node.predictors[final_timepoint])
             else:
-                node.attr["fitness"] = 0.0
+                node.fitness = 0.0
 
     def predict(self, niter = 10, estimate_frequencies = True):
         self.prep_nodes()
