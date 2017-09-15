a27476ecb23338cc2f052485ba0fd3c65e60933a
John Huddleston <huddlej@gmail.com>
2017-09-15 09:58:23 -0700

    Switched indentation to standard depth.

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-        name = "augur",
-        version = "1.0.0",
-        author = "Richard Neher and Trevor Bedford",
-        author_email = "trevor@bedford.io",
-        description = ("Pipeline components for real-time virus analysis"),
-        license = "GNU Affero General Public License v3.0",
-        keywords = "",
-        url = "https://github.com/nextstrain/augur",
-        packages=find_packages(exclude=['docs', 'tests']),
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Topic :: Science",
-            "License :: GNU Affero General Public License v3.0",
-            ],
-        )
+    name = "augur",
+    version = "1.0.0",
+    author = "Richard Neher and Trevor Bedford",
+    author_email = "trevor@bedford.io",
+    description = ("Pipeline components for real-time virus analysis"),
+    license = "GNU Affero General Public License v3.0",
+    keywords = "",
+    url = "https://github.com/nextstrain/augur",
+    packages=find_packages(exclude=['docs', 'tests']),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Science",
+        "License :: GNU Affero General Public License v3.0",
+    ],
+)
