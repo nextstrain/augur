7acdef269205b2f0e8d8b217626600b922111e89
Barney Potter <barney.potter.24@gmail.com>
2018-01-09 16:36:56 -0800

    Bugfix in conditional for rhino submission.

--- builds/flu/run_flu.py
+++ builds/flu/run_flu.py
@@ -33,7 +33,7 @@ def build_live(
                     '--json', 'prepared/flu_%s_%s.json'%(lineage, resolution)]
                 if (system == "qsub"):
                     call = ['qsub', 'submit_script.sh'] + call
-                elif (system = "rhino")
+                elif (system == "rhino")
                     call = ['python'] + call
                     concat = '"' + ' '.join(call) + '"'
                     call = ['sbatch', '-n', '1', '-c', '2', '--mem', '16192', '--time', '12:00:00', '--wrap', concat]
