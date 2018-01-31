beef33c7b93e7cce65b2efe23fcb4c6cb5be28fe
Trevor Bedford <trevor@bedford.io>
2018-01-30 23:33:32 -0800

    Include VIDRL and WHO builds in run_flu

--- builds/flu/run_flu.py
+++ builds/flu/run_flu.py
@@ -1,6 +1,6 @@
 import os
 
-def build_live(
+def run_live(
     lineages = None, resolutions = None,
     system="local",
     frequencies="complete",
@@ -23,7 +23,7 @@ def build_live(
                     '--resolution', resolution,
                     '--segments', " ".join(segments),
                     '--sequences', seq_files,
-                    '--titers', '../../../fauna/data/%s_hi_titers.tsv'%(lineage),
+                    '--titers', '../../../fauna/data/%s_hi_cell_titers.tsv'%(lineage),
                     '--file_prefix', 'flu_%s_*segment*_%s'%(lineage, resolution)]
                 if frequencies == "complete":
                     call = call + ['--complete_frequencies']
@@ -49,68 +49,77 @@ def build_live(
             print(' '.join(call))
             os.system(' '.join(call))
 
-def build_cdc(
-    lineages = None, resolutions = None,
+def run_who(
+    builds = None, lineages = None, resolutions = None,
     system="local",
     frequencies="complete",
     process_na=False,
     no_prepare = False
     ):
+    builds = ['cdc', 'vidrl', 'who'] if builds is None else builds
     lineages = ['h3n2', 'h1n1pdm', 'vic', 'yam'] if lineages is None else lineages
     resolutions = ['2y', '3y', '6y'] if resolutions is None else resolutions
     segments = ['ha', 'na']
 
-    for lineage in lineages:
-        seq_files = " ".join(['../../../fauna/data/%s_%s.fasta'%(lineage, segment)
-                              for segment in segments])
-        for resolution in resolutions:
-            for passage in ['cell', 'egg']:
-                for assay in ['hi', 'fra']:
-                    if lineage!='h3n2' and assay=='fra':
-                        continue
+    for build in builds:
+        for lineage in lineages:
+            seq_files = " ".join(['../../../fauna/data/%s_%s.fasta'%(lineage, segment)
+                                  for segment in segments])
+            for resolution in resolutions:
+                for passage in ['cell', 'egg']:
+                    for assay in ['hi', 'fra']:
+                        if lineage!='h3n2' and assay=='fra':
+                            continue
 
-                    if not (process_na or no_prepare):
-                        call = ['python',
-                            'flu.prepare.py',
-                            '--lineage', lineage,
-                            '--resolution', resolution,
-                            '--segments', " ".join(segments),
-                            '--sequences', seq_files,
-                            '--titers', '../../../fauna/data/%s_cdc_%s_%s_titers.tsv'%(lineage, assay, passage),
-                            '--file_prefix', 'flu_cdc_%s_*segment*_%s_%s_%s'%(lineage, resolution, passage, assay)]
-                        if frequencies == "complete":
-                            call = call + ['--complete_frequencies']
-                        print(' '.join(call))
-                        os.system(' '.join(call))
+                        # temporary, remove when VIDRL titers are fixed for Vic and Yam
+                        if lineage=='vic' and build=='vidrl':
+                            continue
+                        if lineage=='yam' and build=='vidrl':
+                            continue
 
-                    call = [
-                        'flu.process.py',
-                        '--json', 'prepared/flu_cdc_%s_ha_%s_%s_%s.json'%(lineage, resolution, passage, assay),
-                        '--titers_export']
-                    if process_na:
+                        if not (process_na or no_prepare):
+                            call = ['python',
+                                'flu.prepare.py',
+                                '--lineage', lineage,
+                                '--resolution', resolution,
+                                '--segments', " ".join(segments),
+                                '--sequences', seq_files,
+                                '--titers', '../../../fauna/data/%s_%s_%s_%s_titers.tsv'%(lineage, build, assay, passage),
+                                '--file_prefix', 'flu_%s_%s_*segment*_%s_%s_%s'%(build, lineage, resolution, passage, assay)]
+                            if frequencies == "complete":
+                                call = call + ['--complete_frequencies']
+                            print(' '.join(call))
+                            os.system(' '.join(call))
+
                         call = [
                             'flu.process.py',
-                            '--json', 'prepared/flu_cdc_%s_na_%s_%s_%s.json'%(lineage, resolution, passage, assay),
+                            '--json', 'prepared/flu_%s_%s_ha_%s_%s_%s.json'%(build, lineage, resolution, passage, assay),
                             '--titers_export']
+                        if process_na:
+                            call = [
+                                'flu.process.py',
+                                '--json', 'prepared/flu_%s_%s_na_%s_%s_%s.json'%(build, lineage, resolution, passage, assay),
+                                '--titers_export']
 
-                    if (system == "qsub"):
-                        call = ['qsub', 'submit_script.sh'] + call
-                    elif (system == "rhino"):
-                        concat = '"' + ' '.join( ['python'] + call ) + '"'
-                        call = ['sbatch', '-n', '1', '-c', '2', '--mem', '16192', '--time', '12:00:00', '--wrap', concat]
-                    elif (system == "sbatch"):
-                        call = ['sbatch', 'submit_flu.sh'] + call
-                    elif (system == "local"):
-                        call = ['python'] + call
-                    print(' '.join(call))
-                    os.system(' '.join(call))
+                        if (system == "qsub"):
+                            call = ['qsub', 'submit_script.sh'] + call
+                        elif (system == "rhino"):
+                            concat = '"' + ' '.join( ['python'] + call ) + '"'
+                            call = ['sbatch', '-n', '1', '-c', '2', '--mem', '16192', '--time', '12:00:00', '--wrap', concat]
+                        elif (system == "sbatch"):
+                            call = ['sbatch', 'submit_flu.sh'] + call
+                        elif (system == "local"):
+                            call = ['python'] + call
+                        print(' '.join(call))
+                        os.system(' '.join(call))
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Run flu builds')
-    parser.add_argument('-b', '--build', type = str, default = 'live', help='build to run, live or cdc')
+    parser.add_argument('-v', '--version', type = str, default = 'live', help='version to run, live or who')
     parser.add_argument('-s', '--system', type = str, default = 'local', help='where to run, local, qsub or sbatch')
+    parser.add_argument('-b', '--builds', nargs='+', type = str,  help ="flu builds to include")
     parser.add_argument('-l', '--lineages', nargs='+', type = str,  help ="flu lineages to include")
     parser.add_argument('-r', '--resolutions', nargs='+', type = str,  help ="flu resolutions to include")
     parser.add_argument('--frequencies', type = str, default = 'complete', help='frequencies to complete, complete or subsampled')
@@ -118,25 +127,30 @@ if __name__ == '__main__':
     parser.add_argument('--no_prepare', action="store_true", default=False,  help = "rerun previously prepared jsons")
     params = parser.parse_args()
 
+    # only applicable to who version
+    if params.builds is None:
+        params.builds = ['cdc', 'vidrl', 'who']
+
     if params.lineages is None:
         params.lineages = ['h3n2', 'h1n1pdm', 'vic', 'yam']
 
     if params.resolutions is None:
-        if params.build == "live":
+        if params.version == "live":
             params.resolutions = ['2y', '3y', '6y', '12y']
-        elif params.build == "cdc":
+        elif params.version == "who":
             params.resolutions = ['2y', '3y', '6y']
 
-    if params.build == "live":
-        build_live(
+    if params.version == "live":
+        run_live(
             lineages = params.lineages,
             resolutions = params.resolutions,
             system = params.system,
             frequencies = params.frequencies,
             process_na = params.process_na,
             no_prepare = params.no_prepare)
-    elif params.build == "cdc":
-        build_cdc(
+    elif params.version == "who":
+        run_who(
+            builds = params.builds,
             lineages = params.lineages,
             resolutions = params.resolutions,
             system = params.system,
