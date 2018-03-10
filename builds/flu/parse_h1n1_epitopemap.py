0602017d570addfc6246773a4558977de81cd072
Richard Neher <richard.neher@unibas.ch>
2018-03-10 13:58:39 +0100

    removed leading 0 from h1n1 masks, added ha1 and ha1_head masks for h1n1pdm

--- builds/flu/parse_h1n1_epitopemap.py
+++ builds/flu/parse_h1n1_epitopemap.py
@@ -7,8 +7,13 @@ df = pd.read_excel(CDC_h1n1pdm_antigenic_table)
 
 epitope_col = u'Antigenic Site (Caton)'
 
-# there are two extra rows in the file, hence -2
-mask = "".join([ '1' if type(x)==unicode else "0" for x in df.loc[:-2,epitope_col]])
+# there are two extra rows in the file, but this doesn't matter since
+# we restrict to length of the total peptide in augur
+epi_mask = "".join(['1' if type(x)==unicode else "0" for x in df.loc[:,epitope_col]][1:])
+ha1_mask = "".join(['1' if str(x).startswith('HA1') else '0' for x in df.index][1:])
+ha1_head_mask = "".join(['1' if str(x).startswith('HA1_globular_head') else '0' for x in df.index][1:])
 
 with open('metadata/ha_masks.tsv', 'a') as ofile:
-	ofile.write('canton\t'+mask+'\n')
+	ofile.write('canton\t'+epi_mask+'\n')
+	ofile.write('ha1_h1n1pdm\t'+ha1_mask+'\n')
+	ofile.write('HA1_globular_head_h1n1pdm\t'+ha1_head_mask+'\n')
