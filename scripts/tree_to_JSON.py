7c1b8425faf409a3f878d0d77f02ac0cc2a98064
James Hadfield <jh22@sanger.ac.uk>
2018-04-24 18:57:04 -0700

    basic parsing of nexus files

--- scripts/tree_to_JSON.py
+++ scripts/tree_to_JSON.py
@@ -4,14 +4,36 @@ from StringIO import StringIO
 import json
 import numpy as np
 import argparse
+import sys
+import imp
+import logging
+sys.path.append('')
+from base.colorLogging import ColorizingStreamHandler
+
 
 version = 0.1
 
 def get_command_line_args():
     parser = argparse.ArgumentParser(description ="Convert tree files to Auspice ready JSONs. Version {}".format(version))
-    parser.add_argument('--newick', type=str, help="Path to newick file")
-    parser.add_argument('--output_prefix', '-o', required=True, type=str, help="Output prefix (i.e. \"_meta.json\" will be appended to this)")
-    parser.add_argument('--title', default=None, type=str, help="Title (to be displayed by auspice)")
+
+    newick = parser.add_argument_group('newick')
+    newick.add_argument('--newick', type=str, help="Path to newick file")
+
+    beast = parser.add_argument_group('beast')
+    beast.add_argument('--nexus', type=str, help="Path to nexus file")
+    beast.add_argument('--most_recent_tip', type=float, help="Date of the most recent tip (in decimal format)")
+    beast.add_argument('--discrete_traits', type=str, nargs='+', default=[], help="Discrete traits to extract from the BEAST annotations")
+    beast.add_argument('--continuous_traits', type=str, nargs='+', default=[], help="Continuous traits to extract from the BEAST annotations")
+
+    metadata = parser.add_argument_group('metadata')
+
+
+    general = parser.add_argument_group('general')
+    general.add_argument("--maintainer", type=str, nargs=2, default=["", ""], help="Maintaner (display name, link name). Shown in auspice footer.")
+    general.add_argument("--debug", action="store_const", dest="loglevel", const=logging.DEBUG, help="Enable debugging logging")
+    general.add_argument('--output_prefix', '-o', required=True, type=str, help="Output prefix (i.e. \"_meta.json\" will be appended to this)")
+    general.add_argument('--title', default=None, type=str, help="Title (to be displayed by auspice)")
+
     return parser.parse_args()
 
 # Biopython's trees don't store links to node parents, so we need to build
@@ -24,7 +46,7 @@ def all_parents(tree):
             parents[child] = clade
     return parents
 
-def annotate_parents(tree):
+def annotate_phylotree_parents(tree):
     # Get all parent nodes by node.
     parents_by_node = all_parents(tree)
 
@@ -75,6 +97,7 @@ def mock_meta_json(tree, args):
     meta = {}
     meta["updated"] = "today"
     meta["virus_count"] = tree.virus_count
+    meta["maintainer"] = args.maintainer
 
     if not args.title:
         meta["title"] = args.newick
@@ -147,21 +170,90 @@ def make_up_country(tree):
 
 
 if __name__=="__main__":
-    print("***\nThis is a proof of principle script - it should not be relied upon for analysis.\n***\n")
     args = get_command_line_args()
+    root_logger = logging.getLogger('')
+    root_logger.setLevel(args.loglevel if args.loglevel else logging.INFO)
+    root_logger.addHandler(ColorizingStreamHandler())
+    logger = logging.getLogger(__name__)
 
-    tree = Phylo.read(args.newick, "newick");
-    tree.ladderize()
-    tree = annotate_parents(tree)
-    set_basic_information_on_nodes(tree)
-    set_y_values(tree)
-    set_divergence_on_tree(tree)
-    make_up_temporal_data(tree)
-    make_up_country(tree)
+
+    ## PARSE THE TREE ##
+    if args.newick:
+        logger.info("Loading newick tree {}".format(args.newick))
+        tree = Phylo.read(args.newick, "newick");
+        tree.ladderize()
+        tree = annotate_phylotree_parents(tree)
+        set_basic_information_on_nodes(tree)
+        set_y_values(tree)
+        set_divergence_on_tree(tree)
+        make_up_temporal_data(tree)
+        make_up_country(tree)
+    elif args.nexus:
+        try:
+            bt = imp.load_source('baltic', '/Users/james/blab/baltic/baltic.py')
+        except IOError:
+            bt = imp.load_source('baltic', '/Users/evogytis/Documents/BLAB_baltic/baltic.py')
+        logger.info("Loading nexus tree {}".format(args.nexus))
+        bt_tree = bt.loadNexus(args.nexus, absoluteTime=False) ## loads a BEAST nexus file
+        bt_tree.setAbsoluteTime(args.most_recent_tip)
+
+        tree = Phylo.read(StringIO(bt_tree.toString()), 'newick')
+        tree = annotate_phylotree_parents(tree)
+        nodes=tree.get_nonterminals(order='preorder') ## fetch nodes from newick tree
+        bt_nodes=[k for k in bt_tree.traverse_tree(include_all=True) if k.branchType=='node'] ## fetch nodes from baltic tree
+        leaves=tree.get_terminals(order='preorder') ## fetch leaves from newick tree
+        bt_leaves=[k for k in bt_tree.traverse_tree()] ## fetch leaves from baltic tree
+        tree.virus_count = len(leaves)
+
+        # iterate over BT (nexus/BEAST) nodes & leaves as we iterate over the biophylo tree
+        # and transfer relevent data
+        for pairs in [[nodes,bt_nodes],[leaves,bt_leaves]]:
+            for node, b in zip(*pairs):
+                setattr(node,'tvalue',b.height) ## height in years
+                setattr(node,'xvalue',b.height) ## 0.0 in the future?
+                setattr(node,'yvalue',b.y) ## y value after drawing
+                setattr(node,'clade',b.index) ## clade
+                attrs={}
+
+                if b.absoluteTime!=None:
+                    attrs['num_date']=b.absoluteTime
+                else: # root
+                    attrs['num_date']=bt_tree.Objects[0].absoluteTime
+
+                setattr(node,'attr',attrs)
+
+                for trait in args.discrete_traits:
+                    if b.traits.has_key(trait):
+                        attrs[trait] = b.traits[trait]
+                        tset = trait+".set"
+                        tprob = trait+".set.prob"
+                        if b.traits.has_key(tset) and b.traits.has_key(tprob):
+                            attrs[trait+'_confidence']={t:p for t,p in zip(b.traits[tset], b.traits[tprob])}
+
+                for trait in args.continuous_traits:
+                    rate = trait+".rate"
+                    hpd = trait+".rate_95%_HPD"
+                    if b.traits.has_key(rate):
+                        attrs[trait] = b.traits[rate]
+                        if b.traits.has_key(hpd):
+                            attrs[trait+'_confidence'] = b.traits[hpd]
+
+
+        # import pdb; pdb.set_trace()
+
+
 
     tree_json = modified_tree_to_json(tree.root)
     json.dump(tree_json, open("{}_tree.json".format(args.output_prefix), 'w'), indent=2)
     meta_json = mock_meta_json(tree, args)
+
+    ## add the discrete & continous traits as colorBys
+    for trait in args.discrete_traits:
+        meta_json["color_options"][trait] = {"menuItem": trait, "type": "discrete", "legendTitle": trait, "key": trait}
+    for trait in args.continuous_traits:
+        meta_json["color_options"][trait] = {"menuItem": trait, "type": "continous", "legendTitle": trait, "key": trait}
+
+
     json.dump(meta_json, open("{}_meta.json".format(args.output_prefix), 'w'), indent=2)
 
-    print("***\nDONE\n***\n")
+    logger.info("DONE")
