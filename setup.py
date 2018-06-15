60ecda35d0a529842b30315594a605e296b119df
Thomas Sibley <tsibley@fredhutch.org>
2018-06-15 10:08:53 -0700

    Bump modular augur's TreeTime version requirement to match remote
    Now distinguished from the Python 2 version of TreeTime.

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ setup(
             "pytest >=3.2.1, ==3.*",
             "seaborn >=0.6.0, ==0.6.*",
             "tox >=2.8.2, ==2.*",
-            "treetime ==0.3.0"
+            "treetime ==0.4.0"
         ],
         dependency_links = [
-            "https://api.github.com/repos/neherlab/treetime/tarball/py3#egg=treetime-0.3.0"
+            "https://api.github.com/repos/neherlab/treetime/tarball/py3#egg=treetime-0.4.0"
         ],
         classifiers=[
             "Development Status :: 3 - Alpha",
