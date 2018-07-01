3d0eefebfda90593e1c15e4658e29cc30c1ba1c6
Trevor Bedford <trevor@bedford.io>
2018-07-01 11:36:56 -0700

    Upgrade to TreeTime v0.4.1

--- setup.py
+++ setup.py
@@ -24,10 +24,10 @@ setup(
             "seaborn >=0.6.0, ==0.6.*",
             "snakemake >=5.1.5, ==5.*",
             "tox >=2.8.2, ==2.*",
-            "treetime ==0.4.0"
+            "treetime ==0.4.1"
         ],
         dependency_links = [
-            "https://api.github.com/repos/neherlab/treetime/tarball/v0.4.0#egg=treetime-0.4.0"
+            "https://api.github.com/repos/neherlab/treetime/tarball/v0.4.1#egg=treetime-0.4.1"
         ],
         classifiers=[
             "Development Status :: 3 - Alpha",
