f6f5359bedd6e8a27075962235a4356e693438db
Trevor Bedford <trevor@bedford.io>
2018-07-05 15:43:59 -0700

    Create --titer-model option and reduce to single --output

--- augur/titers.py
+++ augur/titers.py
@@ -8,7 +8,7 @@ def run(args):
     T = Phylo.read(args.tree, 'newick')
 
     TM_tree, TM_subs = None, None
-    if args.output_tree_model:
+    if args.titer_model == "tree":
         from .titer_model import TreeModel
         TM_tree = TreeModel(T, args.titers)
         TM_tree.prepare()
@@ -20,13 +20,13 @@ def run(args):
                       'avidity':TM_tree.compile_virus_effects(),
                       'nodes':{n.name:{"dTiter": n.dTiter, "cTiter":n.cTiter}
                                   for n in T.find_clades()}}
-        write_json(tree_model, args.output_tree_model)
+        write_json(tree_model, args.output)
         print("\nInferred titer model of type 'TreeModel' using augur:"
               "\n\tNeher et al. Prediction, dynamics, and visualization of antigenic phenotypes of seasonal influenza viruses."
               "\n\tPNAS, vol 113, 10.1073/pnas.1525578113\n")
-        print("results written to",args.output_tree_model)
+        print("results written to", args.output)
 
-    if args.output_subs_model:
+    if args.titer_model == "substitution":
         from .titer_model import SubstitutionModel
         TM_subs = SubstitutionModel(T, args.titers)
         TM_subs.prepare()
@@ -37,10 +37,9 @@ def run(args):
                       'potency':TM_subs.compile_potencies(),
                       'avidity':TM_subs.compile_virus_effects(),
                       'substitution':TM_subs.compile_substitution_effects()}
-        write_json(subs_model, args.output_subs_model)
+        write_json(subs_model, args.output)
 
         print("\nInferred titer model of type 'SubstitutionModel' using augur:"
               "\n\tNeher et al. Prediction, dynamics, and visualization of antigenic phenotypes of seasonal influenza viruses."
               "\n\tPNAS, vol 113, 10.1073/pnas.1525578113\n")
-        print("results written to",args.output_subs_model)
-
+        print("results written to", args.output)
