8ab85849dff5dceb4f22b6fcecd556f586a8107d
John Huddleston <huddlej@gmail.com>
2018-07-05 16:40:03 -0700

    Added first unit test for KdeFrequencies

---
+++ tests/test_frequencies.py
@@ -0,0 +1,34 @@
+"""
+Unit tests for frequency estimation
+"""
+import json
+import pytest
+
+from ..base.frequencies import KdeFrequencies
+from ..base.io_util import json_to_tree
+
+
+@pytest.fixture
+def tree():
+    """Returns an annotated Bio.Phylo tree.
+    """
+    with open("tests/data/flu_seasonal_h3n2_ha_2y_tree.json") as fh:
+        json_tree = json.load(fh)
+
+    tree = json_to_tree(json_tree)
+    return tree
+
+
+class TestKdeFrequencies(object):
+    """
+    Tests KDE-based frequency estimation methods
+    """
+    def test_estimate(self, tree):
+        """
+        Test frequency estimation with default parameters.
+        """
+        kde_frequencies = KdeFrequencies()
+        frequencies = kde_frequencies.estimate(tree)
+        assert "global" in frequencies
+        assert hasattr(kde_frequencies, "pivots")
+        assert frequencies["global"].values()[0].shape == kde_frequencies.pivots.shape
