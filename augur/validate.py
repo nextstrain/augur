bf08f4ddabbd4fef8908dfd55bac9c7ef3ecab41
James Hadfield <jh22@sanger.ac.uk>
2018-07-06 14:25:25 -0700

    add consistency checks for meta & meta+tree

--- augur/validate.py
+++ augur/validate.py
@@ -1,5 +1,5 @@
 import jsonschema
-import os
+import os, sys
 import json
 from collections import defaultdict
 from pkg_resources import resource_string, resource_filename
@@ -15,24 +15,25 @@ schemaLocations = {
 
 
 def loadJSONsToValidate(paths):
-    ret = []
+    ret = {}
     for path in paths:
         with open(path) as f:
             try:
                 jsonToValidate = json.load(f)
             except json.JSONDecodeError:
-                print("to do JSONDecodeError")
+                raise(Exception("Input JSON Decode Error"))
         if path.endswith("_tree.json"):
             name = "tree"
         elif path.endswith("_meta.json"):
             name = "meta"
         else:
-            raise Exception('unknown name')
-        ret.append({
+            raise Exception('Unknown metadata type: {}'.format(name))
+        if name in ret:
+            raise Exception('Cannot deal with multiple JSONs of the same type ({})'.format(name))
+        ret[name] = {
             "path": path,
-            "name": name,
             "json": jsonToValidate
-        })
+        }
     return ret
 
 def isSchemaValid(schema, name):
@@ -85,38 +86,166 @@ def loadSchemas(names):
 
     return ret
 
+def collectTreeAttrs(root):
+    seen = defaultdict(lambda: {"count": 0, "values": set(), "onAllNodes": False})
+    num_nodes, num_terminal = (0, 0)
+    def recurse(node):
+        nonlocal num_nodes, num_terminal
+        num_nodes += 1
+        for property in node["attr"].keys():
+            seen[property]["count"] += 1
+            value = node["attr"][property]
+            if isinstance(value, str):
+                seen[property]["values"].add(value)
+        if "children" in node:
+            [recurse(child) for child in node["children"]]
+        else:
+            num_terminal += 1
+    recurse(root)
 
-def checkMetaValuesAppearOnTreeNodes():
-    print("checkMetaValuesAppearOnTreeNodes not yet implemented")
+    for data in seen.values():
+        if data["count"] == num_nodes:
+            data["onAllNodes"] = True
+
+    return(seen, num_terminal)
+
+
+def collectAAMutationGenes(root):
+    genes = set()
+    def recurse(node):
+        if "aa_muts" in node and len(node["aa_muts"]):
+            genes.update(node["aa_muts"].keys())
+        if "children" in node:
+            [recurse(child) for child in node["children"]]
+    recurse(root)
+    return genes
+
+
+def verifyMetaJSONIsInternallyConsistent(status, meta):
+    print("Validating that {} is internally consistent... ".format(meta["path"]))
+    j = meta["json"]
+    if "panels" in j and "entropy" in j["panels"] and "annotations" not in j:
+        status = "error"
+        print("\tERROR: The entropy panel has been specified but annotations don't exist.", file=sys.stderr)
+
+    return(status)
+
+
+def verifyMetaAndTreeJSONsAreInternallyConsistent(status, meta, tree):
+    print("Validating that {} and {} are internally consistent... ".format(meta["path"], tree["path"]))
+    mj, tj = (meta["json"], tree["json"])
+    treeAttrs, num_terminal_nodes = collectTreeAttrs(tj)
+
+    def error(msg):
+        nonlocal status
+        status = "error"
+        print("\tERROR: ", msg, file=sys.stderr)
+
+    def warn(msg):
+        nonlocal status
+        if status == "ok":
+            status = "warn"
+        print("\tWARNING: ", msg, file=sys.stderr)
+
+
+    if "geo" in mj:
+        for geoName in mj["geo"]:
+            if geoName not in treeAttrs:
+                error("The geographic resolution \"{}\" does not appear as an attr on any tree nodes.".format(geoName))
+            else:
+                for geoValue in mj["geo"][geoName]:
+                    if geoValue not in treeAttrs[geoName]["values"]:
+                        warn("\"{}\", a value of the geographic resolution \"{}\", does not appear as a value of attr->{} on any tree nodes.".format(geoValue, geoName, geoValue))
+
+    if "color_options" in mj:
+        for colorBy in [x for x in mj["color_options"] if x != "gt"]:
+            if colorBy not in treeAttrs:
+                error("The color_option \"{}\" does not appear as an attr on any tree nodes.".format(colorBy))
+            elif "color_map" in mj["color_options"][colorBy]:
+                for (value, hex) in mj["color_options"][colorBy]["color_map"]:
+                    if value not in treeAttrs[colorBy]["values"]:
+                        warn("Color option \"{}\" specifies a hex code for \"{}\" but this isn't ever seen on the tree nodes.".format(colorBy, value))
+
+    if "filters" in mj:
+        for filter in mj["filters"]:
+            if filter not in treeAttrs:
+                error("The filter \"{}\" does not appear as an attr on any tree nodes.".format(filter))
+
+    if "author_info" in mj:
+        if "authors" not in treeAttrs:
+            warn("\"author_info\" exists in the metadata JSON but \"authors\" are never defined on a tree node attr.")
+        else:
+            for author in mj["author_info"]:
+                if author not in treeAttrs["authors"]["values"]:
+                    warn("Author \"{}\" defined in \"author_info\" but does not appear on any tree node.".format(author))
+            for author in treeAttrs["authors"]["values"]:
+                if author not in mj["author_info"]:
+                    error("Author \"{}\" defined on a tree node but not in \"author_info\".".format(author))
+
+    if "virus_count" in mj and mj["virus_count"] != num_terminal_nodes:
+        error("Meta JSON virus_count ({}) differs from the number of nodes in the tree ({})".format(mj["virus_count"], num_terminal_nodes))
+
+    genes_with_aa_muts = collectAAMutationGenes(tj)
+    if len(genes_with_aa_muts):
+        if "annotations" not in mj:
+            error("The tree defined AA mutations on genes {}, but annotations aren't defined in the meta JSON.".format(", ".join(genes_with_aa_muts)))
+        else:
+            for gene in genes_with_aa_muts:
+                if gene not in mj["annotations"]:
+                    error("The tree defined AA mutations on gene {} which doesn't appear in the metadata annotations object.".format(gene))
+
+
+    return status
 
 
 def run(args):
     '''
     Validate augur produced JSONs
     '''
+    status = "ok" # possible values are "warn" and "error"
     return_code = 0;
-    datasets = loadJSONsToValidate(args.json)
-    schemas = loadSchemas([x["name"] for x in datasets])
 
-    for data in datasets:
+    try:
+        datasets = loadJSONsToValidate(args.json)
+        schemas = loadSchemas([x for x in datasets])
+    except Exception as err:
+        print(err, file=sys.stderr)
+        return 1
+
+    for name, data in datasets.items():
         # make this cmd line controllable
         for schemaType in ["minimal", "recommended"]:
-            if schemaType not in schemas[data["name"]]:
+            if schemaType not in schemas[name]:
                 continue
-            print("Validating {} using the {}/{} schema... ".format(data["path"], data["name"], schemaType), end='')
+            print("Validating {} using the {}/{} schema... ".format(data["path"], name, schemaType), end='')
 
-            validator = schemas[data["name"]][schemaType]
+            validator = schemas[name][schemaType]
             try:
                 validator.validate(data["json"]) # https://python-jsonschema.readthedocs.io/en/latest/validate/
             except jsonschema.exceptions.ValidationError:
-                print("") # new line
-                ret_code = 1
-                msgType = "ERROR" if schemaType is "minimal" else "WARNING"
+                print("FAILED")
+                if schemaType == "minimal":
+                    status = "error"
+                    msgType = "ERROR"
+                elif status == "ok":
+                    status = "warn"
+                    msgType = "WARNING"
+
                 for error in sorted(validator.iter_errors(data["json"]), key=str):
-                    # TODO: make this more meaningful. Can you extract the definition if available?
-                    print("\t", msgType, error.message, "see:", error.schema_path)
+                    trace = list(error.schema_path)
+                    ptrace = " - ".join(trace[-5:] if len(trace) > 6 else trace)
+                    print("\t{}: {}. Trace: {}".format(msgType, error.message, ptrace), file=sys.stderr)
             else:
                 print("SUCCESS")
 
-    checkMetaValuesAppearOnTreeNodes()
-    return return_code
+    if status == "error":
+        print("Skipping consistency checks", file=sys.stderr)
+        return 1
+
+    if "meta" in datasets:
+        status = verifyMetaJSONIsInternallyConsistent(status, datasets["meta"])
+    if "meta" in datasets and "tree" in datasets:
+        status = verifyMetaAndTreeJSONsAreInternallyConsistent(status, datasets["meta"], datasets["tree"])
+
+
+    return 0 if status != "error" else 1
