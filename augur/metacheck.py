41c7d595b70d12dac8ad97a7881e9ced10bdc32f
Emma Hodcroft <emmahodcroft@gmail.com>
2018-09-13 18:18:11 +0200

    gets country/region for city, needs more testing

--- augur/metacheck.py
+++ augur/metacheck.py
@@ -18,19 +18,22 @@ forbidden_characters = [(' ',''), ('(','_'),(')','_'),(':','_'),(',','_'),(';','
 indicators_for_unknown = ['', '?', 'unknown', 'na', 'nan', '-n/a-', '-']
 default_unknown = ''
 
-def determine_standard_columns(columns):
+def determine_standard_columns(columns, country):
     #force user to keep required columns
-    print('Please identify columns containing required meta data. \nThese are the available columns:')
+    print('\n\nPlease identify columns containing required meta data. \nThese are the available columns:')
     for i, j in enumerate(columns):
         print(i, j)
     print(i+1, 'none of them')
     take = {}
-    for field, explanation in [("strain",  " (matches sequence names)"), ("accession",""),
-                               ("date",""), ("country","")]:
+    last_column = ("country", "") if country else ("location", "")
+    standard_columns = [("strain",  " (matches sequence names)"), ("accession",""),
+                               ("date",""), last_column]
+
+    for field, explanation in standard_columns:
         ti = int(input('Which one of your columns is "%s"%s? '%(field, explanation)))
         print()
         if ti<len(columns):
-            take[field] = columns[ti]
+            take[columns[ti]] = field
     return take
 
 
@@ -158,6 +161,14 @@ def read_region_info(region_file=None):
                         region_map[entries[0]] = entries[1]
     return region_map
 
+def read_location_info(location_file):
+    location_country = {}
+    if location_file:
+        with open(location_file, mode='r') as f:
+            for line in f:
+                entries = line.strip().split('\t')
+                location_country[entries[0]] = entries[1]
+    return location_country
 
 def get_synonyms(syn_path):
     synonyms = {}
@@ -250,25 +261,53 @@ def write_synonyms(synonyms, syn_path):
     with open(syn_path, 'w') as the_file:
         the_file.write("\n".join(syn_write))
 
+def get_field_to_use(location):
+    field_to_use = 'city'
+    if 'city' not in location.raw['address']:
+        print("'City' not found in the GeoPy results. Available fields:")
+        avail_fields = {}
+        for i, j in enumerate(location.raw['address']):
+            print(i, j)
+            avail_fields[i] = j
+        field_no = input("Type what to use for 'city': ")
+        field_to_use = avail_fields[int(field_no)]
+    return field_to_use
 
-def locate_place(sample, coor, geolocator, region_dict, regions, synonyms):
-    place_name = sample['country']
-    location = geolocator.geocode((place_name).replace('_',' '), language='en')
+
+def locate_place(sample, coor, geolocator, region_dict, regions, synonyms, country):
+    loc_name = 'country' if country else 'location'
+    place_name = sample[loc_name]
+    location = geolocator.geocode((place_name).replace('_',' '), language='en', addressdetails=True)
     if str(location) == 'None':
         print("The location as formatted couldn't be found in GeoPy")
         answer = 'n'
     else:
-        new_loc = (location.address).lower().replace(' ','_')
         print("\nAn unknown location has been found: ", place_name)
-        print("GeoPy suggests using:", new_loc)
-        answer = input("Is this the right country name, and the name you want to use? y or n: ")
+        if country:
+            new_loc = (location.address).lower().replace(' ','_')
+            print("GeoPy suggests using:", new_loc)
+            answer = input("Is this the right country name, and the name you want to use? y or n: ")
+        else:
+            field_to_use = get_field_to_use(location)
+            new_loc = location.raw['address'][field_to_use].lower()
+            countryVal = location.raw['address']['country'].lower()
+            print("GeoPy suggests using: %s with country: %s"%(new_loc, countryVal))
+            answer = input("Is this the right location and country, and the location name you'd like to use? y or n: ")
 
     if answer.lower() == 'y':
-        sample['country'] = new_loc
+        sample['country'] = new_loc if country else countryVal
+        if not country:
+            sample['location'] = new_loc
         if new_loc not in coor:
             lon = location.longitude
             lat = location.latitude
             coor[new_loc] = [new_loc, 'XX', lat, lon]
+        if not country and countryVal not in coor:
+            country_latlong = geolocator.geocode(countryVal, language='en', addressdetails=True)
+            lon = country_latlong.longitude
+            lat = country_latlong.latitude
+            country_code = location.raw['address']['country_code'].upper()
+            coor[new_loc] = [new_loc, country_code, lat, lon]
 
         region_questionnaire(sample, region_dict, regions)
 
@@ -277,9 +316,11 @@ def locate_place(sample, coor, geolocator, region_dict, regions, synonyms):
         redo = True
         while redo == True:
             # print(place_name)
-            new_loc = (input("Type the correct country name or 'n' if unknown: ")).lower().replace(' ','_')
+            new_loc = (input("Type the correct name or 'n' if unknown: ")).lower().replace(' ','_')
             if new_loc in ['n', 'na']:
                 indicators_for_unknown.append(place_name)
+                if not country:
+                    sample['location'] = default_unknown
                 sample['country'] = default_unknown
                 sample['region'] = default_unknown
 
@@ -288,7 +329,7 @@ def locate_place(sample, coor, geolocator, region_dict, regions, synonyms):
                 if new_loc in synonyms.keys():
                     new_loc = synonyms[new_loc]
 
-                sample['country'] = new_loc
+                sample[loc_name] = new_loc
 
                 if new_loc in region_dict: #what was typed, or synonym of what was typed, is in geo
                     sample['region'] = region_dict[new_loc]
@@ -299,10 +340,11 @@ def locate_place(sample, coor, geolocator, region_dict, regions, synonyms):
                         redo = False
 
                     elif new_loc != 'n' and new_loc not in coor:
-                        location = geolocator.geocode((new_loc).replace('_',' '), language='en')
-                        print('would you like to: \n1. Use the coordinates for this location?', location.address)
-                        print('2. Enter coordinates manually in the file after this run')
-                        print('3. Type the country again')
+                        location = geolocator.geocode((new_loc).replace('_',' '), language='en', addressdetails=True)
+                        coord_text = "coordinates" if country else "coordinates/country"
+                        print('Would you like to: \n1. Use the %s for this location?'%(coord_text), location.address)
+                        print('2. Enter %s manually in the file after this run'%(coord_text))
+                        print('3. Type the location again')
                         loc_ok = input('1, 2, or 3: ')
                         if loc_ok == '1':
                             lon = location.longitude
@@ -310,18 +352,28 @@ def locate_place(sample, coor, geolocator, region_dict, regions, synonyms):
                             coor[new_loc] = [new_loc, 'XX', lat, lon]
                             redo = False
                         elif loc_ok == '2':
-                            print('update the new longitude and latitude manually in the geo_long_lat file')
+                            if not country:
+                                new_country = input("Please type the country you'd like to use: ")
+                                sample['country'] = new_country
+                            region_questionnaire(sample, region_dict, regions)
+                            print('Please update the new longitude and latitude manually in the geo_long_lat file')
                             redo = False
                         #else 3, allow user to type again
 
                         if loc_ok != '3':
+                            new_name = (location.address).lower().replace(' ','_')
+                            if not country:
+                                field_to_use = get_field_to_use(location)
+                                new_name = location.raw['address'][field_to_use].lower().replace(' ','_')
                             print('What name would you like to use?')
                             print('1: ', new_loc)
-                            print('2: ', (location.address).lower().replace(' ','_'))
+                            print('2: ', new_name)
                             name_loc = input('1 or 2: ')
                             if name_loc == '2':
-                                sample['country'] = (location.address).lower().replace(' ','_')
-                                new_loc = sample['country']
+                                sample[loc_name] = new_name
+                                new_loc = sample[loc_name]
+                            if not country:
+                                sample['country'] = location.raw['address']['country'].lower()
                             region_questionnaire(sample, region_dict, regions)
 
 
@@ -333,18 +385,30 @@ def run(args):
     #Read in meta-data provided by user
     orig_meta = pd.read_csv(args.input, sep="\t" if args.input.endswith('.tsv') else r'\s*,\s*')
 
+    country = True
+    #See if they want to do by Country or smaller location
+    print('Would you like to identify geography by country or more specific location?')
+    print('1. Country')
+    print('2. More specific (city, county, state)')
+    ti = int(input('Type your choice (default 1): '))
+    if ti==2:
+        country = False
+
     #Get user to identify columns, select those to keep
-    column_map = determine_standard_columns(orig_meta.columns)
+    column_map = determine_standard_columns(orig_meta.columns, country)
 
     #Pick other columns to keep
-    column_map.update(determine_columns_to_keep([c for c in orig_meta.columns if c not in column_map.values()]))
+    columns_to_keep = determine_columns_to_keep([c for c in orig_meta.columns if c not in column_map.keys()])
+    column_map.update(columns_to_keep)
 
     ## properties section would go here - leaving out for now.
     ##### (this standardizes your host names, etc)
     #####
 
-    new_meta = orig_meta.loc[:, column_map.values()].rename(column_map)
+    new_meta = orig_meta.loc[:, column_map.keys()].rename(index=str, columns=column_map)
     new_meta['region'] = 0
+    if not country:
+        new_meta['country'] = 0
     samples = {}
     for ri, r in new_meta.iterrows():
         if r.strain not in samples:
@@ -389,6 +453,12 @@ def run(args):
     else:
         synonyms = {}
 
+    #if available, read in file linking location to country
+    if not country:
+        location_country = {}
+        if args.location_country:
+            location_country = read_location_info(args.location_country)
+
     # Get lat-longs - needs to be adapted to new format!!
     #coor = pd.read_csv(args.geo_info, sep='\t')
     coordinates = read_lat_longs()  # should use this instead! But needs adapting
@@ -398,30 +468,38 @@ def run(args):
 
     for s,v in samples.items():
         wrong_name = ""
+        loc_name = 'country' if country else 'location'
         #country = v['country']
 
-        if type(v['country']) == str:
-           v['country'] = v['country'].lower().replace(' ','_')
+        if type(v[loc_name]) == str:
+           v[loc_name] = v[loc_name].lower().replace(' ','_')
         else:
-            v['country'] = default_unknown
+            v[loc_name] = default_unknown
 
-        if v['country'] in region_dict:
+        if v[loc_name] in region_dict:
+            v['region'] = region_dict[v[loc_name]]
+        elif v[loc_name] in indicators_for_unknown:
+            v[loc_name] = default_unknown
+            v[loc_name] = default_unknown
+        elif not country and v[loc_name] in location_country:
+            v['country'] = location_country[v[loc_name]]
             v['region'] = region_dict[v['country']]
-        elif v['country'] in indicators_for_unknown:
-            v['country'] = default_unknown
-            v['region'] = default_unknown
-        elif v['country'] in synonyms:
-            v['country'] = synonyms[v['country']]
-            v['region'] = region_dict[v['country']]
+        elif v[loc_name] in synonyms:
+            v[loc_name] = synonyms[v[loc_name]] #if country else location_country[synonyms[v[loc_name]]]
+            if not country:
+                v['country'] =  location_country[v[loc_name]]
+            v['region'] = region_dict[v[loc_name]] if country else region_dict[v['country']]
         else: # country not the region dict nor in list of synonyms nor unknown
-            wrong_name = v['country']
-            locate_place(v, coordinates, geolocator,region_dict, region_colors.keys(), synonyms)
+            wrong_name = v[loc_name]
+            locate_place(v, coordinates, geolocator,region_dict, region_colors.keys(), synonyms, country)
 
-            print(wrong_name)
-            s_add = input("\nWas this country (%s) misspelled (should it be added to synonyms)? y or n: " % wrong_name)
-            if s_add.lower() == 'y':
-                synonyms[wrong_name] = v['country']
-
+            #print(wrong_name)
+            if wrong_name != v[loc_name]: #don't offer to add to synonyms if identical...
+                s_add = input("Was this location (%s) misspelled (should it be added to synonyms)? y or n: " % wrong_name)
+                if s_add.lower() == 'y':
+                    synonyms[wrong_name] = v[loc_name]
+            if not country:
+                location_country[v[loc_name]] = v['country']
 
     countries_current = {v['country'] for v in samples.values() if v['country']}
     regions_current = {v['region'] for v in samples.values() if v['country']}
@@ -468,11 +546,18 @@ def run(args):
     #synonyms
     write_synonyms(synonyms, args.synonyms)
 
+    #write out location dict
+    write_out_loc_file = input("\nWould you like to save a file linking your locations and countries? [y] or n: ")
+    if write_out_loc_file != 'n':
+        geo_file_name = args.geo_regions.split("/")[-1]
+        location_file = args.geo_regions.replace(geo_file_name, "location_country.tsv")
+        (pd.DataFrame.from_dict(data=location_country, orient='index').to_csv(location_file, sep='\t', header=False))
+
     #lat-long
     #TODO: Need to decide if we want to update the augur lat-long, or just a local version?
     #Does this not do anything??
-    with open(args.geo_info, 'w') as outfile:
-        outfile.write(coordinates.to_csv(sep='\t', index=False, header=coordinates.columns))
+    #with open(args.geo_info, 'w') as outfile:
+    #    outfile.write(coordinates.to_csv(sep='\t', index=False, header=coordinates.columns))
 
     #if include hosts/properties someday, do here
 
