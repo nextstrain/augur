ee0611a0a8d5b4c44fae70dd6e2d63066891a8d0
Emma Hodcroft <emmahodcroft@gmail.com>
2018-12-03 15:21:45 +0100

    reconstruct seq works with VCF

--- augur/reconstruct_sequences.py
+++ augur/reconstruct_sequences.py
@@ -17,7 +17,7 @@ def register_arguments(parser):
     parser.add_argument('--genes', nargs='+', help="genes to translate (list or file containing list)")
     parser.add_argument('--mutations', required=True, type=str, help="json file containing mutations "
                             "mapped to each branch and the sequence of the root.")
-    parser.add_argument('--vcf-reference', type=str, help='fasta file of the sequence the VCF was mapped to')
+    parser.add_argument('--vcf-aa-reference', type=str, help='fasta file of the reference gene translations for VCF format')
     parser.add_argument('--internal-nodes', action='store_true', help="include sequences of internal nodes in output")
     parser.add_argument('--output', type=str)
 
@@ -58,17 +58,27 @@ def run(args):
     else:
         genes = args.genes
 
+    #check whether VCF - by existance of VCF reference file
+    is_vcf = args.vcf_aa_reference is not None
 
     ## check file format and read in sequences
-    is_vcf = False
     node_data = read_node_data(args.mutations, args.tree)
     if node_data is None:
-        print("ERROR: could not read mutation data (incl sequences)")
+        print("ERROR: could not read mutation data "+ ("(incl sequences)" if not is_vcf else ""))
         return 1
 
+    root_node = tree.root.name
+
+    #if VCF, read in the reference seq for each gene, put on root
+    if(is_vcf):
+        node_data["nodes"][root_node]['aa_sequences'] = {}
+        with open(args.vcf_aa_reference) as handle:
+            for record in SeqIO.parse(handle, "fasta"):
+                if record.id in args.genes:
+                    #'root' may not be same as 'reference', so apply any mutations at root here!
+                    node_data["nodes"][root_node]['aa_sequences'][record.id] = get_sequence(str(record.seq), node_data["nodes"][root_node]["aa_muts"][record.id])
 
     # check that root node has sequences for each requested gene
-    root_node = tree.root.name
     if "aa_sequences" not in node_data["nodes"][root_node]:
         print("ERROR: ancestral sequences are not provided")
         return 1
