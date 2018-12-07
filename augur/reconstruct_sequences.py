9be0cd37cbeaaaa9cbd9ab33d7c1d4b04b1fd998
Richard Neher <richard.neher@unibas.ch>
2018-12-07 10:41:12 +0100

    augur/reconstruct_sequences.py: remove the %GENE template for output, make rule accept single gene name only, one output file

--- augur/reconstruct_sequences.py
+++ augur/reconstruct_sequences.py
@@ -7,14 +7,13 @@ import numpy as np
 from collections import defaultdict
 from Bio import SeqIO, Seq, SeqRecord, Phylo
 from .utils import read_node_data, write_json
-from .translate import get_genes_from_file
 from treetime.vcf_utils import read_vcf
 
 
 
 def register_arguments(parser):
     parser.add_argument('--tree', required=True, help="tree as Newick file")
-    parser.add_argument('--genes', nargs='+', help="genes to translate (list or file containing list)")
+    parser.add_argument('--gene', type=str, help="gene to translate (list or file containing list)")
     parser.add_argument('--mutations', required=True, type=str, help="json file containing mutations "
                             "mapped to each branch and the sequence of the root.")
     parser.add_argument('--vcf-aa-reference', type=str, help='fasta file of the reference gene translations for VCF format')
@@ -52,12 +51,6 @@ def run(args):
     ## read tree and data, if reading data fails, return with error code
     tree = Phylo.read(args.tree, 'newick')
 
-    # If genes is a file, read in the genes to translate
-    if args.genes and len(args.genes) == 1 and os.path.isfile(args.genes[0]):
-        genes = get_genes_from_file(args.genes[0])
-    else:
-        genes = args.genes
-
     #check whether VCF - by existance of VCF reference file
     is_vcf = args.vcf_aa_reference is not None
 
@@ -74,7 +67,7 @@ def run(args):
         node_data["nodes"][root_node]['aa_sequences'] = {}
         with open(args.vcf_aa_reference) as handle:
             for record in SeqIO.parse(handle, "fasta"):
-                if record.id in args.genes:
+                if record.id==args.gene:
                     #'root' may not be same as 'reference', so apply any mutations at root here!
                     node_data["nodes"][root_node]['aa_sequences'][record.id] = get_sequence(str(record.seq), node_data["nodes"][root_node]["aa_muts"][record.id])
 
@@ -82,24 +75,23 @@ def run(args):
     if "aa_sequences" not in node_data["nodes"][root_node]:
         print("ERROR: ancestral sequences are not provided")
         return 1
-    if not all([g in node_data["nodes"][root_node]['aa_sequences'] for g in genes]):
-        print("ERROR: ancestral sequences missing for some genes")
+    if not args.gene in node_data["nodes"][root_node]['aa_sequences']:
+        print("ERROR: ancestral sequences missing for gene",args.gene)
         return 1
 
     # gather all reconstructed sequences
-    sequences = defaultdict(dict)
+    sequences = dict()
     is_terminal = {}
-    for g in genes:
-        sequences[g][root_node] = node_data["nodes"][root_node]['aa_sequences'][g]
-        is_terminal[root_node] = False
-        for node in tree.get_nonterminals(order='preorder'):
-            parent_sequence = sequences[g][node.name]
-            for child in node:
-                sequences[g][child.name] = get_sequence(parent_sequence, node_data["nodes"][child.name]["aa_muts"][g])
-                is_terminal[child.name] = child.is_terminal()
+
+    sequences[root_node] = node_data["nodes"][root_node]['aa_sequences'][args.gene]
+    is_terminal[root_node] = False
+    for node in tree.get_nonterminals(order='preorder'):
+        parent_sequence = sequences[node.name]
+        for child in node:
+            sequences[child.name] = get_sequence(parent_sequence, node_data["nodes"][child.name]["aa_muts"][args.gene])
+            is_terminal[child.name] = child.is_terminal()
 
     # write alignments to file
-    for g in genes:
-        seqs = [SeqRecord.SeqRecord(seq=Seq.Seq(sequences[g][strain]), id=strain, name=strain, description='')
-                for strain in sequences[g] if is_terminal[strain] or args.internal_nodes]
-        SeqIO.write(seqs, args.output.replace('%GENE', g), 'fasta')
+    seqs = [SeqRecord.SeqRecord(seq=Seq.Seq(sequences[strain]), id=strain, name=strain, description='')
+            for strain in sequences if is_terminal[strain] or args.internal_nodes]
+    SeqIO.write(seqs, args.output, 'fasta')
