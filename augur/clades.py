6f5d2a229a73643514dcb891d0b52a881e6438b8
Richard Neher <richard.neher@unibas.ch>
2019-05-23 17:25:19 +0200

    augur/clades.py: change clade assignment from mutations to sequences to avoid unassigned nodes when reference sequence with respect to which mutations are defined is not part of the build

--- augur/clades.py
+++ augur/clades.py
@@ -5,6 +5,7 @@ Assign clades to nodes in a tree based on amino-acid or nucleotide signatures.
 import sys
 from Bio import Phylo
 import pandas as pd
+import numpy as np
 from .utils import get_parent_name_by_child_name_for_tree, read_node_data, write_json
 
 def read_in_clade_definitions(clade_file):
@@ -23,7 +24,7 @@ def read_in_clade_definitions(clade_file):
 
     df = pd.read_csv(clade_file, sep='\t' if clade_file.endswith('.tsv') else ',')
     for index, row in df.iterrows():
-        allele = (row.gene, row.site, row.alt)
+        allele = (row.gene, row.site-1, row.alt)
         if row.clade in clades:
             clades[row.clade].append(allele)
         else:
@@ -61,16 +62,19 @@ def get_node_alleles(node_name, muts, parents):
 
     return node_alleles
 
-def is_node_in_clade(clade_alleles, node_alleles):
+def is_node_in_clade(clade_alleles, node):
     '''
     Determines whether a node contains all mutations that define a clade
     '''
-    is_clade = False
     # mutations are stored on nodes in format 'R927H' matching the clade definitions
     # if all of the clade-defining mutations are in the node mutations, it's part of this clade
-    if all([ clade_allele in node_alleles for clade_allele in clade_alleles ]):
-        is_clade = True
-    return is_clade
+    conditions = []
+    for gene, pos, state in clade_alleles:
+        if gene=='nuc':
+            conditions.append(node.sequence[pos]==state)
+        else:
+            conditions.append(node.aa_sequences[gene][pos]==state)
+    return all(conditions)
 
 def assign_clades(clade_designations, all_muts, tree):
     '''
@@ -87,19 +91,53 @@ def assign_clades(clade_designations, all_muts, tree):
     for node in tree.find_clades(order = 'preorder'):
         clade_membership[node.name] = {'clade_membership': 'unassigned'}
 
+
+    # count leaves
+    for node in tree.find_clades(order = 'postorder'):
+        node.leaf_count = 1 if node.is_terminal() else np.sum([c.leaf_count for c in node])
+
+    # attach sequences to root
+    try:
+        tree.root.sequence = list(all_muts[tree.root.name]["sequence"])
+    except:
+        raise TypeError("augur.clades requires nucleotide mutation files with full sequences for the root node.")
+
+    if "aa_muts" in all_muts[tree.root.name]:
+        try:
+            tree.root.aa_sequences = {gene:list(seq) for gene, seq in all_muts[tree.root.name]["aa_sequences"].items()}
+        except:
+            raise TypeError("augur.clades requires amino acid mutation files with full sequences for the root node.")
+
+    # attach sequences to all nodes
+    for node in tree.get_nonterminals(order='preorder'):
+        for c in node:
+            c.sequence = node.sequence.copy()
+            for mut in all_muts[c.name]['muts']:
+                a, pos, d = mut[0], int(mut[1:-1])-1, mut[-1]
+                c.sequence[pos] = d
+            if hasattr(node, 'aa_sequences'):
+                c.aa_sequences = {}
+                for gene in node.aa_sequences:
+                    c.aa_sequences[gene] = node.aa_sequences[gene].copy()
+                    for mut in all_muts[c.name]['aa_muts'][gene]:
+                        a, pos, d = mut[0], int(mut[1:-1])-1, mut[-1]
+                        c.aa_sequences[gene][pos] = d
+
+
     # second pass to assign 'clade_annotation' to basal nodes within each clade
     # if multiple nodes match, assign annotation to largest
     # otherwise occasional unwanted cousin nodes get assigned the annotation
     for clade_name, clade_alleles in clade_designations.items():
-        node_counts = {}
+        node_counts = []
         for node in tree.find_clades(order = 'preorder'):
-            node_alleles = get_node_alleles(node.name, all_muts, parents)
-            if is_node_in_clade(clade_alleles, node_alleles):
-                node_counts[node.name] = node.count_terminals()
-        sorted_nodes = list(sorted(node_counts.items(), key=lambda x: x[1], reverse=True))
+            if is_node_in_clade(clade_alleles, node):
+                node_counts.append(node)
+        sorted_nodes = sorted(node_counts, key=lambda x: x.leaf_count, reverse=True)
+        print(clade_name, clade_alleles, len(sorted_nodes))
         if len(sorted_nodes) > 0:
-            target_node = sorted_nodes[0][0]
-            clade_membership[target_node] = {'clade_annotation': clade_name, 'clade_membership': clade_name}
+            target_node = sorted_nodes[0]
+            print(target_node.name, clade_name)
+            clade_membership[target_node.name] = {'clade_annotation': clade_name, 'clade_membership': clade_name}
 
     # third pass to propagate 'clade_membership'
     # don't propagate if encountering 'clade_annotation'
