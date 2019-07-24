75bd262de347cbeddaedd94493cf153d11748303
Trevor Bedford <trevor@bedford.io>
2019-07-24 16:47:16 -0700

    Significantly relax version requirements on base packages
    Here, I aimed for compatibility back to ~2016 and tested each of these versions using the zika-tutorial augur build, as well as builds in tests/ directory. Everything tested passes with these minimal versions.
    
    The only explicit breakpoint I ran into was BioPython 1.66, which errored out with "translate() got an unexpected keyword argument 'gap'" in `augur translate`. BioPython 1.67 resolved this.
    
    It may be possible to push others back further, but I don't think necessary based on timeframes.
    
    I left "full" and "dev" alone with this testing / this commit.

--- setup.py
+++ setup.py
@@ -25,20 +25,20 @@ setup(
     url = "https://github.com/nextstrain/augur",
     project_urls = {
         "Bug Reports": "https://github.com/nextstrain/augur/issues",
-        "Change Log":  "https://github.com/nextstrain/augur/blob/master/CHANGES.md#next",
-        "Source":      "https://github.com/nextstrain/augur",
+        "Change Log": "https://github.com/nextstrain/augur/blob/master/CHANGES.md#next",
+        "Source": "https://github.com/nextstrain/augur",
     },
     packages = ['augur'],
     package_data = {'augur': ['data/*']},
     data_files = [("", ["LICENSE.txt"])],
     python_requires = '>=3.4',
     install_requires = [
-        "bcbio-gff >=0.6.4, ==0.6.*",
-        "biopython >=1.73, ==1.*",
-        "jsonschema ==3.0.0a3",
-        "pandas >=0.23.4",
-        "phylo-treetime >=0.6.2, ==0.6.*",
-        "snakemake >=5.5.4, ==5.*"
+        "bcbio-gff >=0.6.0, ==0.6.*",
+        "biopython >=1.67, ==1.*",
+        "jsonschema >=3.0.0, ==3.*",
+        "pandas >=0.20.0, ==0.*",
+        "phylo-treetime >=0.5.6, <0.7",
+        "snakemake >=5.4.0, ==5.*"
     ],
     extras_require = {
         'full': [
@@ -49,12 +49,12 @@ setup(
         'dev': [
             "pylint >=1.7.6, ==1.7.*",
             "pytest >=3.2.1, ==3.*",
-            "recommonmark >=0.5.0",
-            "Sphinx >=2.0.1",
-            "sphinx-argparse >=0.2.5",
-            "sphinx-rtd-theme >=0.4.3",
+            "recommonmark >=0.5.0, ==0.*",
+            "Sphinx >=2.0.1, ==2.*",
+            "sphinx-argparse >=0.2.5, ==0.*",
+            "sphinx-rtd-theme >=0.4.3, ==0.*",
             "wheel >=0.32.3, ==0.32.*",
-            "ipdb >=0.10.1"
+            "ipdb >=0.10.1, ==0.*"
         ]
     },
     classifiers = [
