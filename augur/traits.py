324666e6ff67b6d1e567d2d15e3986ee91eae9e7
Richard Neher <richard.neher@unibas.ch>
2020-01-03 22:42:07 +0100

    traits: use treetime's reconstruct_discrete_traits function instead of the parallel implementation that existed in augur traits.

--- augur/traits.py
+++ augur/traits.py
@@ -10,7 +10,7 @@ from .utils import read_metadata, write_json, get_json_name
 TINY = 1e-12
 
 def mugration_inference(tree=None, seq_meta=None, field='country', confidence=True,
-                        infer_gtr=True, root_state=None, missing='?', sampling_bias_correction=None):
+                        missing='?', sampling_bias_correction=None):
     """
     Infer likely ancestral states of a discrete character assuming a time reversible model.
 
@@ -24,10 +24,6 @@ def mugration_inference(tree=None, seq_meta=None, field='country', confidence=Tr
         meta data field to use
     confidence : bool, optional
         calculate confidence values for inferences
-    infer_gtr : bool, optional
-        infer a GTR model for trait transitions (otherwises uses a flat model with rate 1)
-    root_state : None, optional
-        force the state of the root node (currently not implemented)
     missing : str, optional
         character that is to be interpreted as missing data, default='?'
 
@@ -40,95 +36,60 @@ def mugration_inference(tree=None, seq_meta=None, field='country', confidence=Tr
     alphabet : dict
         mapping of character states to
     """
-    from treetime import GTR
+    from treetime.wrappers import reconstruct_discrete_traits
     from Bio.Align import MultipleSeqAlignment
     from Bio.SeqRecord import SeqRecord
     from Bio.Seq import Seq
     from Bio import Phylo
 
     T = Phylo.read(tree, 'newick')
+    traits = {}
     nodes = {n.name:n for n in T.get_terminals()}
-
-    # Determine alphabet only counting tips in the tree
-    places = set()
     for name, meta in seq_meta.items():
         if field in meta and name in nodes:
-            places.add(meta[field])
-    if root_state is not None:
-        places.add(root_state)
+            traits[name] = meta[field]
+    unique_states = list(set(traits.values()))
 
-    # construct GTR (flat for now). The missing DATA symbol is a '-' (ord('-')==45)
-    places = sorted(places)
-    nc = len(places)
-    if nc>180:
-        print("ERROR: geo_inference: can't have more than 180 places!", file=sys.stderr)
-        return None,None,None
-    elif nc==0:
-        print("ERROR: geo_inference: list of places is empty!", file=sys.stderr)
-        return None,None,None
-    elif nc==1:
-        print("WARNING: geo_inference: only one place found -- set every internal node to %s!"%places[0], file=sys.stderr)
-        alphabet = {'A':places[0]}
-        alphabet_values = ['A']
-        gtr = None
+    if len(unique_states)==0:
+        print("WARNING: no states found for discrete state reconstruction.")
         for node in T.find_clades():
-            node.sequence=['A']
-            node.marginal_profile=np.array([[1.0]])
+            node.__setattr__(field, None)
+        return T, None, {}
+    elif len(unique_states)==1:
+        print("WARNING: only one state found for discrete state reconstruction:", unique_states)
+        for node in T.find_clades():
+            node.__setattr__(field, unique_states[0])
+        return T, None, {}
+    elif len(unique_states)<180:
+        tt, letter_to_state, reverse_alphabet = \
+            reconstruct_discrete_traits(T, traits, missing_data=missing,
+                 sampling_bias_correction=sampling_bias_correction)
     else:
-        # set up model
-        alphabet = {chr(65+i):place for i,place in enumerate(places)}
-        model = GTR.custom(pi = np.ones(nc, dtype=float)/nc, W=np.ones((nc,nc)),
-                          alphabet = np.array(sorted(alphabet.keys())))
+        print("ERROR: 180 or more distinct discrete states found. TreeTime is currently not set up to handle that many states.")
+        sys.exit(1)
 
-        missing_char = chr(65+nc)
-        alphabet[missing_char]=missing
-        model.profile_map[missing_char] = np.ones(nc)
-        model.ambiguous = missing_char
-        alphabet_rev = {v:k for k,v in alphabet.items()}
-
-        # construct pseudo alignment
-        pseudo_seqs = []
-        for name, meta in seq_meta.items():
-            if name in nodes:
-                s=alphabet_rev[meta[field]] if field in meta else missing_char
-                pseudo_seqs.append(SeqRecord(Seq(s), name=name, id=name))
-        aln = MultipleSeqAlignment(pseudo_seqs)
-
-        # set up treetime and infer
-        from treetime import TreeAnc
-        tt = TreeAnc(tree=tree, aln=aln, gtr=model, convert_upper=False, verbose=0)
-        tt.use_mutation_length = False
-        tt.infer_ancestral_sequences(infer_gtr=infer_gtr, store_compressed=False, pc=1.0,
-                                     marginal=True, normalized_rate=False)
-
-        if sampling_bias_correction:
-            tt.gtr.mu *= sampling_bias_correction
-            tt.infer_ancestral_sequences(infer_gtr=False, store_compressed=False,
-                                         marginal=True, normalized_rate=False)
-
-        T = tt.tree
-        gtr = tt.gtr
-        alphabet_values = tt.gtr.alphabet
-
+    if tt is None:
+        print("ERROR in discrete state reconstruction in TreeTime. Please look for errors above.")
+        sys.exit(1)
 
     # attach inferred states as e.g. node.region = 'africa'
-    for node in T.find_clades():
-        node.__setattr__(field, alphabet[node.sequence[0]])
+    for node in tt.tree.find_clades():
+        node.__setattr__(field, letter_to_state[node.sequence[0]])
 
     # if desired, attach entropy and confidence as e.g. node.region_entropy = 0.03
     if confidence:
-        for node in T.find_clades():
+        for node in tt.tree.find_clades():
             pdis = node.marginal_profile[0]
             S = -np.sum(pdis*np.log(pdis+TINY))
 
-            marginal = [(alphabet[alphabet_values[i]], pdis[i]) for i in range(len(alphabet_values))]
+            marginal = [(letter_to_state[tt.gtr.alphabet[i]], pdis[i]) for i in range(len(tt.gtr.alphabet))]
             marginal.sort(key=lambda x: x[1], reverse=True) # sort on likelihoods
             marginal = [(a, b) for a, b in marginal if b > 0.001][:4] #only take stuff over .1% and the top 4 elements
             conf = {a:b for a,b in marginal}
             node.__setattr__(field + "_entropy", S)
             node.__setattr__(field + "_confidence", conf)
 
-    return T, gtr, alphabet
+    return tt.tree, tt.gtr, letter_to_state
 
 
 def register_arguments(parser):
@@ -182,7 +143,8 @@ def run(args):
     out_prefix = '.'.join(args.tree.split('.')[:-1])
     for column in args.columns:
         T, gtr, alphabet = mugration_inference(tree=tree_fname, seq_meta=traits,
-                                               field=column, confidence=args.confidence, sampling_bias_correction=args.sampling_bias_correction)
+                                               field=column, confidence=args.confidence,
+                                               sampling_bias_correction=args.sampling_bias_correction)
         if T is None: # something went wrong
             continue
 
