950765b4a2f144dbf5033c231d346875d195750d
Carol Willing <carolcode@willingconsulting.com>
2020-03-27 16:00:17 -0700

    add clades test file

---
+++ tests/test_clades.py
@@ -0,0 +1,38 @@
+"""Tests for clades assignment to nodes in a tree"""
+import pytest
+
+
+def test_assign_clades():
+    pass
+
+
+def test_get_reference_sequence():
+    pass
+
+
+def test_is_node_in_clade():
+    pass
+
+
+def test_read_in_clade_definitions_valid_file_type():
+    with data / titer_model / h3n2_titers_subset.tsv:
+
+    pass
+
+
+def test_read_in_clade_definitions_invalid_file_type():
+    with data/titer_model/h3n2_titers_subset.tsv:
+
+    pass
+
+
+def test_read_in_clade_definition_no_file():
+    pass
+
+
+def test_register_arguments_for_command_options():
+    pass
+
+
+def test_run_clades():
+    pass
