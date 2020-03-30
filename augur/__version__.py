dc87410437e6179bb3985da9ebbe0db6864eaa49
Ryan Grout <ryan@ryangrout.org>
2020-03-30 10:43:59 -0500

    Add version_info tuple

--- augur/__version__.py
+++ augur/__version__.py
@@ -1,1 +1,10 @@
+from collections import namedtuple
+
+__all__ = ['__version__', 'version_info']
+
+__vinfo = namedtuple('version_info', ['major', 'minor', 'micro'])
+
 __version__ = '6.4.3'
+version_info = __vinfo(*map(int, __version__.split('.')))
+
+del __vinfo
