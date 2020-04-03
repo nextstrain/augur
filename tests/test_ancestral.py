3c8a5497055b7f658dcfe6d8bdd287bc6908fead
vanguard737 <vanguard737@gmail.com>
2020-04-03 08:28:44 -0500

    Adds test for ancestral.run(), using data from add_to_alignment build.

--- tests/test_ancestral.py
+++ tests/test_ancestral.py
@@ -6,6 +6,27 @@ from unittest import mock
 
 
 class TestAncestral:
+    def test_add_to_alignment_run(self):
+        TEST_DATA_DIR = "./tests/data/ancestral/add_to_alignment/"
+        args = argparse.Namespace(
+            tree=TEST_DATA_DIR + "in/tree.nwk",
+            alignment=TEST_DATA_DIR + "in/aligned.fasta",
+            output_node_data=None,
+            output=TEST_DATA_DIR + "out/nt_muts.json",
+            output_sequences=None,
+            inference="joint",
+            vcf_reference=None,
+            output_vcf=None,
+            infer_ambiguous=True,
+            keep_overhangs=False,
+            version=None,
+        )
+        result = ancestral.run(args)
+        assert result == 0
+        nt_muts_expected = json.load(open(TEST_DATA_DIR + "expected/nt_muts.json", "r"))
+        nt_muts_actual = json.load(open(TEST_DATA_DIR + "out/nt_muts.json", "r"))
+        assert nt_muts_actual == nt_muts_expected
+
     def test_zika_run(self):
         TEST_DATA_DIR = "./tests/data/ancestral/zika/"
         args = argparse.Namespace(
