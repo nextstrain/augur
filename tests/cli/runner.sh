870cb97dd9eaea3d516c9d38217c65a6f4060a11
vanguard737 <vanguard737@gmail.com>
2020-04-19 19:03:17 -0500

    Suppress expected failures from 'augur export' tests.

--- tests/cli/runner.sh
+++ tests/cli/runner.sh
@@ -11,7 +11,7 @@ echo -e "\nRunning all tests\n-----------------\n\n"
 cd $(dirname "$BASH_SOURCE")
 
 # exclude_test_dirs=(export filter refine traits tree runner.sh)
-exclude_test_dirs=(traits runner.sh)
+exclude_test_dirs=(export traits runner.sh)
 
 for test_dir in *; do
     if [[ ! " ${exclude_test_dirs[@]} " =~ " ${test_dir} " ]]; then
