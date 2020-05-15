532bd4556942df9ac0a4c79340f6699da3fca7c1
vanguard737 <vanguard737@gmail.com>
2020-05-15 06:32:56 -0500

    This commit does the following:
    - Adds a Pytest wrapper, tests/test_cram.py, to execute all Cram CLI tests located under tests/.
    - Updates .travis.yml to execute this test independently from existing Augur unit tests, and to generate a coverage report showing code coverage *just* for the Cram CLI tests.  Augur's unit tests generate their own coverage report and upload it to Codecov.  To keep Cram from stepping on that unit-test coverage report, we just write these results to terminal.
    - Updates pytest.python3.ini to exclude the pytest Cram wrapper when executing unit tests.  This should keep the Cram tests' pytest coverage report from stepping on the unit-test coverage report.

---
+++ tests/test_cram.py
@@ -0,0 +1,41 @@
+import pytest
+import pathlib
+import cram
+import os
+
+# Coverage.py warning: No data was collected. (no-data-collected)
+import augur
+
+
+# Locate all Cram tests to be executed.
+test_dir = "./tests/"
+cram_tests = list(pathlib.Path(test_dir).glob("**/*.t"))
+
+
+def get_ids(args):
+    """
+    Convert PosixPaths representing individual Cram test files to string.
+    This allows Pytest to include each Cram test's filepath and success/failure status in the pytest output.
+    """
+    return str(args)
+
+
+class TestCram:
+    """
+    Pytest wrapper for Cram CLI tests.
+    This test executes each Cram test defined under tests/, and fails if Cram returns any diffs.
+
+    To generate a coverage report for all Cram CLI tests, execute this command:
+        pytest -vv -s tests/test_cram.py --cov-report=term --cov augur
+    """
+
+    @pytest.mark.parametrize("cram_test_file", cram_tests, ids=get_ids)
+    def test_all(self, tmpdir, cram_test_file):
+        # cram.main() sets these environment variables, but cram.testfile() does not.
+        # If they're unset, cram tests can't (e.g.) write to $TMP/out from here.
+        for s in ("TMPDIR", "TEMP", "TMP"):
+            os.environ[s] = str(tmpdir)
+        # cram expects a bytes literal here, e.g. b"tests/cli/ancestral/add_to_alignment/ancestral/ancestral.t"
+        ins, outs, diffs = cram.testfile(path=bytes(cram_test_file), shell="/bin/bash")
+        diff_list = list(diffs)
+        assert len(diff_list) == 0, f"Cram diffs: {diff_list}"
