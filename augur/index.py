a2ee0344546b6f528e7481d1c4bd6b04242663a9
John Huddleston <huddlej@gmail.com>
2020-12-31 14:40:20 -0800

    Prototype augur filter sequence index interface
    Adds an optional `--sequence-index` argument to augur filter that
    accepts the output of the new `augur index` command and uses these
    sequence composition data to filter sequences by length or invalid
    nucleotide composition instead of inspecting sequences directly.
    
    This prototype interface is backwards-compatible with previous workflows
    that don't precalculate the sequence index. As such, it tries to
    calculate the index in the fly when one is not provided by the user.
    This commit stubs out the functions in `index.py` that would allow this
    on-the-fly calculation.
    
    The proposed `index_sequences` function would contain the current body
    of the `augur index` module's `run` function and call the proposed
    `index_sequence` function on each input sequence. The separate
    `index_sequence` allows us to test the most important logic of the index
    module with doctests or unit tests. This commit provides some doctests
    as examples of how this function could work.

--- augur/index.py
+++ augur/index.py
@@ -3,11 +3,70 @@ Count occurrence of bases in a set of sequences
 """
 
 from Bio import SeqIO
+import Bio.Seq
+import Bio.SeqRecord
 import csv
 
 def register_arguments(parser):
     parser.add_argument('--sequences', '-s', required=True, help="sequences in fasta or VCF format")
     parser.add_argument('--output', '-o', help="output file", required=True)
+
+
+def index_sequence(sequence, values):
+    """Count the number of nucleotides for a given sequence record.
+
+    Parameters
+    ----------
+    sequence : Bio.SeqRecord.SeqRecord
+        sequence record to index.
+
+    values : list of lists of str
+        values to count
+
+    Returns
+    -------
+    list :
+        summary of the given sequence's strain name, length, nucleotide counts
+        for the given values, and a final column with the number of characters
+        that didn't match any of those in the given values.
+
+    >>> other_IUPAC = ['r', 'y', 's', 'w', 'k', 'm', 'd', 'h', 'b', 'v']
+    >>> values = [['a'],['c'],['g'],['t'],['n'], other_IUPAC, ['-'], ['?']]
+    >>> sequence_a = Bio.SeqRecord.SeqRecord(seq=Bio.Seq.Seq("ACTGN-?XWN"), id="seq_A")
+    >>> index_sequence(sequence_a, values)
+    ['seq_A', 10, 1, 1, 1, 1, 2, 2, 1, 1, 0]
+
+    >>> sequence_b = Bio.SeqRecord.SeqRecord(seq=Bio.Seq.Seq("ACTGACTG"), id="seq_B")
+    >>> index_sequence(sequence_b, values)
+    ['seq_B', 8, 2, 2, 2, 2, 0, 0, 0, 0, 0]
+
+    Characters in the given sequence that are not in the given list of values to
+    count get counted in the final column.
+
+    >>> sequence_c = Bio.SeqRecord.SeqRecord(seq=Bio.Seq.Seq("ACTG%@!!!NN"), id="seq_C")
+    >>> index_sequence(sequence_c, values)
+    ['seq_C', 11, 1, 1, 1, 1, 1, 0, 0, 0, 5]
+
+    """
+    pass
+
+
+def index_sequences(sequences_path, sequence_index_path):
+    """Count the number of A, C, T, G, N, other IUPAC nucleotides, ambiguous bases
+    ("?" and "-"), and other invalid characters in a set of sequences and write
+    the composition as a data frame to the given sequence index path.
+
+    Parameters
+    ----------
+    sequences_path : str or Path-like
+        path to a sequence file to index.
+
+    sequence_index_path : str or Path-like
+        path to a tab-delimited file containing the composition details for each
+        sequence in the given input file.
+
+    """
+    pass
 
 
 def run(args):
