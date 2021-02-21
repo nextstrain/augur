158287038a1ba869e82a7ee1eaebc6e176429c6b
Joseph Tobing <joseph.h.tobing@gmail.com>
2021-02-21 13:17:52 +0700

    fix: displayed developer documentation in augur table of contents

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
-    'contribute/DEV_DOCS.md',
     'faq/colors.md',
     'faq/fasta_input.md',
     'faq/import-beast.md',
