a85194c243db8d85e6fc06ea2d614e0b6095a0c4
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-02-02 10:01:53 -0800

    Update supported Python versions to drop 3.6, include 3.9 and 3.10 (#822)
    This PR updates supported Python versions based on [current EOL status](https://devguide.python.org/#status-of-python-branches).
    
    - Add variable for Augur version associated with min Python version
    - Update CI matrix and minimum version references
    - Bump Augur version associated with min Python version to 14
    
    Ref #793

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from pathlib    import Path
 import setuptools
 import sys
 
-min_version = (3, 6)
+py_min_version = (3, 7)  # minimal supported python version
+since_augur_version = (14, 0)  # py_min_version is required since this augur version
 
-if sys.version_info < min_version:
+if sys.version_info < py_min_version:
     error = """
-Beginning with augur 7.0.0, Python {0} or above is required.
+Beginning with augur {0}, Python {1} or above is required.
+You are using Python {2}.
 
 This may be due to an out of date pip.
 
 Make sure you have pip >= 9.0.1.
-""".format('.'.join(str(n) for n in min_version)),
+""".format('.'.join(str(n) for n in since_augur_version),
+           '.'.join(str(n) for n in py_min_version),
+           '.'.join(str(n) for n in sys.version_info[:3]))
     sys.exit(error)
 
 base_dir = Path(__file__).parent.resolve()
@@ -45,7 +49,7 @@ setuptools.setup(
     },
     packages = setuptools.find_packages(),
     package_data = {'augur': ['data/*']},
-    python_requires = '>={}'.format('.'.join(str(n) for n in min_version)),
+    python_requires = '>={}'.format('.'.join(str(n) for n in py_min_version)),
     install_requires = [
         "bcbio-gff >=0.6.0, ==0.6.*",
         "biopython >=1.67, !=1.77, !=1.78",
@@ -87,9 +91,10 @@ setuptools.setup(
 
         # Python 3 only
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     # Install an "augur" program which calls augur.__main__.main()
     #   https://setuptools.readthedocs.io/en/latest/setuptools.html#automatic-script-creation
