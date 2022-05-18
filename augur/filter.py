8232839d335bd392ae0b27bc6fa01a49eb751145
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-05-18 15:14:44 -0700

    [DROP FOR 937] Move FilterException to filter_support.exceptions

--- augur/filter.py
+++ augur/filter.py
@@ -17,6 +17,8 @@ import sys
 from tempfile import NamedTemporaryFile
 from typing import Collection
 
+from augur.filter_support.exceptions import FilterException
+
 from .dates import numeric_date, numeric_date_type, SUPPORTED_DATE_HELP_TEXT, is_date_ambiguous, get_numerical_dates
 from .errors import AugurError
 from .index import index_sequences, index_vcf
@@ -96,12 +98,6 @@ def register_parser(parent_subparsers):
     parser = parent_subparsers.add_parser("filter", help=__doc__)
     register_arguments(parser)
     return parser
-
-
-class FilterException(AugurError):
-    """Representation of an error that occurred during filtering.
-    """
-    pass
 
 
 def read_priority_scores(fname):
@@ -945,7 +941,7 @@ def get_groups_for_subsampling(strains, metadata, group_by=None):
     >>> get_groups_for_subsampling(strains, metadata, group_by)
     Traceback (most recent call last):
       ...
-    augur.filter.FilterException: The specified group-by categories (['missing_column']) were not found.
+    augur.filter_support.exceptions.FilterException: The specified group-by categories (['missing_column']) were not found.
 
     If we try to group by some columns that exist and some that don't, we allow
     grouping to continue and print a warning message to stderr.
---
+++ augur/filter_support/exceptions.py
@@ -0,0 +1,7 @@
+from augur.errors import AugurError
+
+
+class FilterException(AugurError):
+    """Representation of an error that occurred during filtering.
+    """
+    pass
