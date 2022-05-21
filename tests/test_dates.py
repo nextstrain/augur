1178e4f47898242f3fc7e1f58b39ca01d1be9b4f
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-05-20 17:46:31 -0700

    Add pytests for various date formats

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -2,7 +2,15 @@ import datetime
 import pytest
 from freezegun import freeze_time
 from augur import dates
+from augur.dates import InvalidDateFormat, numeric_date
 from augur.errors import AugurError
+
+
+def get_numeric_min_max(date):
+    return (
+        numeric_date(date, 'min'),
+        numeric_date(date, 'max'),
+    )
 
 
 class TestDates:
@@ -84,3 +92,124 @@ class TestDates:
         }
         with pytest.raises(AugurError):
             dates.get_numerical_dates(metadata)
+
+    def test_ambiguous_day(self):
+        """Ambiguous day yields a certain min/max range."""
+        date_min, date_max = get_numeric_min_max("2018-01-XX")
+        assert date_min == pytest.approx(2018.001, abs=1e-3)
+        assert date_max == pytest.approx(2018.083, abs=1e-3)
+
+    def test_missing_day(self):
+        """Date without day yields a range equivalent to ambiguous day."""
+        date_min, date_max = get_numeric_min_max("2018-01")
+        assert date_min == pytest.approx(2018.001, abs=1e-3)
+        assert date_max == pytest.approx(2018.083, abs=1e-3)
+
+    def test_ambiguous_month(self):
+        """Ambiguous month yields a certain min/max range."""
+        date_min, date_max = get_numeric_min_max("2018-XX-XX")
+        assert date_min == pytest.approx(2018.001, abs=1e-3)
+        assert date_max == pytest.approx(2018.999, abs=1e-3)
+
+    def test_missing_month(self):
+        """Date without month/day yields a range equivalent to ambiguous month/day."""
+        date_min, date_max = get_numeric_min_max("2018")
+        assert date_min == pytest.approx(2018.001, abs=1e-3)
+        assert date_max == pytest.approx(2018.999, abs=1e-3)
+
+    def test_numerical_exact_year(self):
+        """Numerical year ending in .0 should be interpreted as exact."""
+        date_min, date_max = get_numeric_min_max("2018.0")
+        assert date_min == pytest.approx(2018.001, abs=1e-3)
+        assert date_max == pytest.approx(2018.001, abs=1e-3)
+
+    def test_ambiguous_year(self):
+        """Ambiguous year replaces X with 0 (min) and 9 (max)."""
+        date_min, date_max = get_numeric_min_max("201X-XX-XX")
+        assert date_min == pytest.approx(2010.001, abs=1e-3)
+        assert date_max == pytest.approx(2019.999, abs=1e-3)
+
+    def test_ambiguous_year_incomplete_date(self):
+        """Ambiguous year without month/day yields a range equivalent to ambiguous month/day counterpart."""
+        date_min, date_max = get_numeric_min_max("201X")
+        assert date_min == pytest.approx(2010.001, abs=1e-3)
+        assert date_max == pytest.approx(2019.999, abs=1e-3)
+
+    @pytest.mark.parametrize(
+        "date",
+        [
+            ("201X-01-01"),
+            ("201X-01-XX"),
+            ("201X-XX-01"),
+            ("2010-XX-01"),
+        ],
+    )
+    def test_invalid_ambiguity_between_date_parts(self, date):
+        """Test various forms of invalid ambiguity between date parts."""
+        with pytest.raises(InvalidDateFormat):
+            get_numeric_min_max(date)
+
+    @pytest.mark.parametrize(
+        "date",
+        [
+            ("20X0"),
+            ("20X0-XX-XX"),
+            ("2010-X1"),
+            ("2010-X1-XX"),
+            ("2010-01-X0"),
+        ],
+    )
+    def test_invalid_ambiguity_within_date_part(self, date):
+        """Test various forms of invalid ambiguity within date parts."""
+        with pytest.raises(InvalidDateFormat):
+            get_numeric_min_max(date)
+
+    def test_ambiguous_year_lowercase_x(self):
+        """Ambiguous year with a lowercase x raises an error."""
+        with pytest.raises(InvalidDateFormat):
+            get_numeric_min_max("201x")
+
+    @pytest.mark.parametrize(
+        "date",
+        [
+            ("2018-00-01"),
+            ("2018-13-01"),
+            ("2018-01-00"),
+            ("2018-02-30"),
+        ],
+    )
+    def test_out_of_bounds_error(self, date):
+        """Out-of-bounds month/day cannot be parsed."""
+        with pytest.raises(InvalidDateFormat):
+            get_numeric_min_max(date)
+
+    @pytest.mark.parametrize(
+        "date",
+        [
+            ("-2018-01-01"),
+            ("-2018-XX-XX"),
+            ("-2018-01"),
+            ("-2018"),
+        ],
+    )
+    def test_negative_iso_date_error(self, date):
+        """All forms of negative ISO dates are unsupported."""
+        with pytest.raises(InvalidDateFormat):
+            get_numeric_min_max(date)
+
+    def test_negative_numeric_date(self):
+        """Parse negative numeric date."""
+        date_min, date_max = get_numeric_min_max("-2018.0")
+        assert date_min == pytest.approx(-2018.0, abs=1e-3)
+        assert date_max == pytest.approx(-2018.0, abs=1e-3)
+
+    def test_zero_year_incomplete_error(self):
+        """Zero year-only date is unsupported."""
+        with pytest.raises(InvalidDateFormat):
+            get_numeric_min_max("0")
+
+    def test_zero_year_exact(self):
+        """Parse the date 0.0."""
+        date_min, date_max = get_numeric_min_max("0.0")
+        assert date_min == pytest.approx(0.0, abs=1e-3)
+        assert date_max == pytest.approx(0.0, abs=1e-3)
