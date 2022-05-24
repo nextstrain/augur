1f3e8940f5eaf810dfcf0f2e089107a7d10bb6c3
Victor Lin <13424970+victorlin@users.noreply.github.com>
2022-05-24 09:43:06 -0700

    Run Cram for all Cram test files under tests/

--- run_tests.sh
+++ run_tests.sh
@@ -22,7 +22,7 @@ python3 -m pytest -c pytest.python3.ini $coverage_arg "$@"
 # Only run functional tests if we are not running a subset of tests for pytest.
 if [ "$partial_test" = 0 ]; then
     echo "Running functional tests with cram"
-    cram --shell=/bin/bash tests/functional/*.t tests/builds/*.t
+    cram --shell=/bin/bash tests/
 else
     echo "Skipping functional tests when running a subset of unit tests"
 fi
