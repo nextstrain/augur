1f0d8234549ada754264f2469d21d3cf51a96a66
Jover <joverlee521@gmail.com>
2022-07-12 09:23:34 -0700

    Refactor import_ and import_beast to a package
    Moves the import_ and import_beast modules to an import_ package and
    uses the new `add_command_subparsers` function to add the beast
    subparser.
    
    Search via cs.github.com¹ shows that there are no external uses of
    the `augur.import` or `augur.import_beast` APIs, so this should be
    safe to move to a package.
    
    ¹ https://cs.github.com/?scopeName=All+repos&scope=&q=%22augur.import%22

--- augur/import_/__init__.py
+++ augur/import_/__init__.py
@@ -1,18 +1,18 @@
 """
 Import analyses into augur pipeline from other systems
 """
-from .utils import first_line
-from .import_beast import run_beast, register_arguments_beast
+from augur.argparse_ import add_command_subparsers
+from augur.utils import first_line
+from . import beast
+
+SUBCOMMANDS = [
+    beast,
+]
 
 def register_parser(parent_subparsers):
     parser = parent_subparsers.add_parser("import", help=first_line(__doc__))
     metavar_msg = "Import analyses into augur pipeline from other systems"
     subparsers = parser.add_subparsers(title="TYPE",
                                        metavar=metavar_msg)
-    subparsers.required = True
-    register_arguments_beast(subparsers)
+    add_command_subparsers(subparsers, SUBCOMMANDS)
     return parser
-
-def run(args):
-    if "beast" in args:
-        return run_beast(args)
