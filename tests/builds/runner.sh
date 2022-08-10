98e8622ebf2829fced60fd0dfa21ac8bbc56c063
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-08-10 15:12:22 +0200

    chore: format .sh script

--- tests/builds/runner.sh
+++ tests/builds/runner.sh
@@ -1,4 +1,3 @@
-
 function errorFound {
   echo -e "\nTest Script Failed at Line $1"
   exit 2
@@ -11,20 +10,18 @@ echo -e "Running all tests\n-----------------\n\n"
 cd $(dirname "$BASH_SOURCE")
 
 if [ -d ./auspice ]; then
-    rm -rf auspice
+  rm -rf auspice
 fi
 mkdir auspice
 
-
 for snakefile in ./*/Snakefile; do
-    echo -e "\nRunning ${snakefile} (quietly)\n"
-    pushd $(dirname "${snakefile}") >/dev/null
-    snakemake --cores 1 clean --quiet 1>/dev/null
-    snakemake --cores 1 --quiet 1>/dev/null
-    cp auspice/*.json ../auspice
-    popd >/dev/null
+  echo -e "\nRunning ${snakefile} (quietly)\n"
+  pushd $(dirname "${snakefile}") >/dev/null
+  snakemake --cores 1 clean --quiet 1>/dev/null
+  snakemake --cores 1 --quiet 1>/dev/null
+  cp auspice/*.json ../auspice
+  popd >/dev/null
 done
 
 echo -e "\nAll tests passed. You can view the results by running"
 echo -e "auspice view --datasetDir $(pwd)/auspice"
-
