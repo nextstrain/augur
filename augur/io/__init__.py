5d4632b9e09b8101e500d94817c59cddfaa79f97
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-08-23 10:44:55 -0700

    flake8: Prohibit unused imports
    The preceding commits addressed most violations of this rule. The
    remaining violations are intentional, so add inline exceptions for those.
    
    I would have added a file-wide exception, however the only option for
    that is a file-wide exception to all errors, not just one error.

--- augur/io/__init__.py
+++ augur/io/__init__.py
@@ -2,6 +2,6 @@
 """
 # Functions and variables exposed here are part of Augur's public Python API.
 # To use functions internally, import directly from the submodule.
-from .file import open_file
-from .metadata import read_metadata
-from .sequences import read_sequences, write_sequences
+from .file import open_file  # noqa: F401
+from .metadata import read_metadata  # noqa: F401
+from .sequences import read_sequences, write_sequences  # noqa: F401
