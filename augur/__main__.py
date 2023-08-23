3dae1bfafe89c79318ce086d6cbe51de73c316e6
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-08-23 11:27:58 -0700

    Configure output streams to support non-Unicode encodings
    Fixes a bug where Unicode characters in Augur's help output would result
    in an unhelpful error. This isn't expected to happen for most users. At
    the expense of those users, keep using Unicode characters; for them,
    print replacement characters.

--- augur/__main__.py
+++ augur/__main__.py
@@ -2,11 +2,16 @@
 Stub function and module used as a setuptools entry point.
 """
 
+import sys
 import augur
 from sys import argv, exit
 
 # Entry point for setuptools-installed script and bin/augur dev wrapper.
 def main():
+    # Support non-Unicode encodings by replacing Unicode characters instead of erroring.
+    sys.stdout.reconfigure(errors="backslashreplace")
+    sys.stderr.reconfigure(errors="backslashreplace")
+
     return augur.run( argv[1:] )
 
 # Run when called as `python -m augur`, here for good measure.
