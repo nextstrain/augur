acc4913b4e65ab0a12362590258aa21bf09c42a8
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-01-31 14:59:26 -0800

    Allow custom delimiter, header, columns to TabularFile
    To be used in future commits.

--- augur/io/tabular_file.py
+++ augur/io/tabular_file.py
@@ -13,7 +13,7 @@ class InvalidDelimiter(Exception):
 
 
 class TabularFile(File):
-    """Represents a tabular file with a delimiter and required header line.
+    """Represents a tabular file with a delimiter and optional header line.
 
     A pandas DataFrame can provide these properties, however this class is more
     suitable for large files as it does not store the rows in memory.
@@ -28,31 +28,59 @@ class TabularFile(File):
     delimiter: str
     """Inferred delimiter of file."""
 
-    def __init__(self, path: str, delimiters: Sequence[str] = None):
+    header: bool
+    """Whether the first of the file represents column names."""
+
+    def __init__(self, path: str, delimiter: str = None, delimiters: Sequence[str] = None,
+                 header: bool = True, columns: Sequence[str] = None):
         """
         Parameters
         ----------
         path
             Path of the tabular file.
+        delimiter
+            Use this as the delimiter.
         delimiters
             Possible delimiters to use, in order of precedence.
+        header
+            If true, the first line will be used as column names and not a row of data.
+        columns
+            If set, this will be used for column names.
         """
         super().__init__(path)
 
-        if delimiters is None:
-            delimiters = DEFAULT_DELIMITERS
+        if delimiter:
+            # Delimiter is given as an argument.
+            if delimiters:
+                raise ValueError("At most one of delimiter and delimiters can be set.")
+            self.delimiter = delimiter
+        else:
+            # Delimiter is inferred from possible delimiters.
+            if not delimiters:
+                delimiters = DEFAULT_DELIMITERS
 
-        # Infer the dialect.
-        self.delimiter = get_delimiter(self.path, delimiters)
+            # Infer the dialect.
+            self.delimiter = get_delimiter(self.path, delimiters)
 
-        # Infer the column names.
-        with self.open() as f:
-            reader = csv.reader(f, delimiter=self.delimiter)
-            try:
-                self.columns = next(reader)
-            except StopIteration:
-                raise AugurError(f"{self.path}: Expected a header row but it is empty.")
-
+        if header:
+            # Infer column names from the header.
+            if columns:
+                raise ValueError("Tabular file must have either a header row or column names specified, but not both.")
+            else:
+                with self.open() as f:
+                    reader = csv.reader(f, delimiter=self.delimiter)
+                    try:
+                        self.header = True
+                        self.columns = next(reader)
+                    except StopIteration:
+                        raise AugurError(f"{self.path}: Expected a header row but it is empty.")
+        else:
+            # Column names should be given as an argument.
+            if not columns:
+                raise ValueError("Tabular file must have either a header row or column names specified.")
+            else:
+                self.header = False
+                self.columns = columns
 
     def rows(self, strict: bool = True):
         """Yield rows in a dictionary format. Empty lines are ignored.
@@ -65,8 +93,8 @@ class TabularFile(File):
         with self.open() as f:
             reader = csv.DictReader(f, delimiter=self.delimiter, fieldnames=self.columns, restkey=None, restval=None)
 
-            # Skip the header row.
-            next(reader)
+            if self.header:
+                next(reader)
 
             # NOTE: Empty lines are ignored by csv.DictReader.
             # <https://github.com/python/cpython/blob/647b6cc7f16c03535cede7e1748a58ab884135b2/Lib/csv.py#L181-L185>
