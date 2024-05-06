8c9f247f24386ec19628094e8dc042b7cc78bf37
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-05-06 14:46:14 -0700

    Add docstring to print_err
    Clarify when this function should be used.

--- augur/io/print.py
+++ augur/io/print.py
@@ -2,4 +2,6 @@ import sys
 
 
 def print_err(*args):
+    """Print to stderr. When data goes to stdout (most cases), this should be
+    used for any informational messages, not just errors/warnings."""
     print(*args, file=sys.stderr)
