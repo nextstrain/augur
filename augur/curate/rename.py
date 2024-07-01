fb54c4c2c5e8bc6dfe03f938960a0e3b4bf11a04
james hadfield <hadfield.james@gmail.com>
2024-07-01 15:07:19 +1200

    [curate rename] refactor to match curate structure
    Includes changes to make the copied script work as a new Augur
    subcommand and fit in with the codebase. The functional behaviour of the
    actual renaming is unchanged, although the I/O options are now expanded
    as we inherit the general `augur curate` machinery.

--- augur/curate/rename.py
+++ augur/curate/rename.py
@@ -1,28 +1,31 @@
 """
-Renames fields of the NDJSON record from stdin and outputs modified records
-to stdout.
+Renames fields / columns of the input data
 """
+
+from typing import Iterable
+from augur.io.print import print_err
 import argparse
-import json
-from sys import stderr, stdin, stdout
 
+def register_parser(parent_subparsers):
+    parser = parent_subparsers.add_parser("rename",
+    parents = [parent_subparsers.shared_parser],
+    help = __doc__)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument("--field-map", nargs="+",
+    p = parser.add_argument_group(title="RENAME SPECIFIC OPTIONS")
+
+    p.add_argument("--field-map", nargs="+", default=[],
         help="Fields names in the NDJSON record mapped to new field names, " +
              "formatted as '{old_field_name}={new_field_name}'. " +
              "If the old field does not exist in record, the new field will be added with an empty string value. " +
              "If the new field already exists in record, then the renaming of the old field will be skipped. " +
              "Skips the field if the old field name is the same as the new field name (case-sensitive).")
-    parser.add_argument("--force", action="store_true",
+    p.add_argument("--force", action="store_true",
         help="Force renaming of old field even if the new field already exists. " +
              "Please keep in mind this will overwrite the value of the new field.")
 
-    args = parser.parse_args()
+    return parser
+
+def run(args: argparse.Namespace, records: Iterable[dict]) -> Iterable[dict]:
 
     field_map = {}
     for field in args.field_map:
@@ -33,20 +36,18 @@ if __name__ == '__main__':
 
         field_map[old_name] = new_name
 
-    for record in stdin:
-        record = json.loads(record)
+    for record in records:
+        record = record.copy()
 
         for old_field, new_field in field_map.items():
 
             if record.get(new_field) and not args.force:
-                print(
+                print_err(
                     f"WARNING: skipping rename of {old_field} because record",
-                    f"already has a field named {new_field}.",
-                    file=stderr
+                    f"already has a field named {new_field}."
                 )
                 continue
 
             record[new_field] = record.pop(old_field, '')
 
-        json.dump(record, stdout, allow_nan=False, indent=None, separators=',:')
-        print()
+        yield(record)
