nextstrain/augur

commit 041412c5ec6f8a8458150520b4ea0e5bb7d4857e

Use standard date parsing function in subsampling
Replace the one-off YYYY-MM-DD format parsing with a function that is
more widely used across the codebase and supports more formats.

This requires adding support for the YYYY-MM format in the function.

augur/dates/__init__.py
@@ -4,8 +4,8 @@ from textwrap import dedent
 import isodate
 import pandas as pd
 import re
-from treetime.utils import numeric_date as tt_numeric_date
-from typing import Dict, Tuple, Union
+from treetime.utils import numeric_date as tt_numeric_date, datetime_from_numeric
+from typing import Any, Dict, Optional, Tuple, Union
 from augur.errors import AugurError
 from .errors import InvalidDate
 
@@ -131,6 +131,11 @@ Matches:
 2. Other positive integers (e.g. 1, 123, 12345)
 """
 
+RE_YEAR_MONTH_ONLY = re.compile(r'^\d{4}-\d{2}$')
+"""
+Matches reduced precision ISO 8601 dates that are missing the day part (e.g. 2018-03).
+"""
+
 RE_AUGUR_AMBIGUOUS_DATE = re.compile(r'.*XX.*')
 """
 Matches an Augur-style ambiguous date with 'XX' used to mask unknown parts of the date.
@@ -146,6 +151,14 @@ def get_numerical_date_from_value(value, fmt, min_max_year=None) -> Union[float,
     if RE_YEAR_ONLY.match(value):
         # year-only date is ambiguous
         value = fmt.replace('%Y', value).replace('%m', 'XX').replace('%d', 'XX')
+
+    if RE_YEAR_MONTH_ONLY.match(value):
+        # Note: this is already ISO 8601 so format (fmt) is ignored.
+        try:
+            start, end = AmbiguousDate(f"{value}-XX", fmt="%Y-%m-%d").range(min_max_year=min_max_year)
+        except InvalidDate as error:
+            raise AugurError(str(error)) from error
+        return (date_to_numeric(start), date_to_numeric(end))
 
     if RE_AUGUR_AMBIGUOUS_DATE.match(value):
         try:
@@ -187,3 +200,31 @@ def get_year_month(year, month):
 def get_year_week(year, month, day):
     year, week = datetime.date(year, month, day).isocalendar()[:2]
     return f"{year}-{str(week).zfill(2)}"
+
+def get_year_month_day(value: Any) -> Tuple[Optional[int], Optional[int], Optional[int]]:
+    """
+    Extract year, month, and day from a date value.
+
+    Individual date components can be None if unresolvable.
+    """
+    date_or_range = get_numerical_date_from_value(value, fmt="%Y-%m-%d")
+
+    if date_or_range is None:
+        return (None, None, None)
+
+    # Date range
+    elif isinstance(date_or_range, tuple):
+        start, end = (datetime_from_numeric(date_or_range[0]),
+                      datetime_from_numeric(date_or_range[1]))
+
+        # Only use unambiguous date components
+        year = start.year if start.year == end.year else None
+        month = start.month if start.month == end.month else None
+        day = start.day if start.day == end.day else None
+
+        return (year, month, day)
+
+    # Exact date
+    else:
+        dt = datetime_from_numeric(date_or_range)
+        return (dt.year, dt.month, dt.day)

augur/filter/subsample.py
@@ -7,7 +7,7 @@ import pandas as pd
 from textwrap import dedent
 from typing import Collection, Dict, Iterable, List, Optional, Set, Tuple, Union
 
-from augur.dates import get_year_month, get_year_week
+from augur.dates import get_year_month, get_year_week, get_year_month_day
 from augur.errors import AugurError
 from augur.io.metadata import METADATA_DATE_COLUMN
 from augur.io.print import print_err, _n
@@ -124,11 +124,11 @@ def get_groups_for_subsampling(strains, metadata, group_by=None):
             # to generate other columns, and will be discarded at the end.
             temp_prefix = str(uuid.uuid4())
             temp_date_cols = [f'{temp_prefix}year', f'{temp_prefix}month', f'{temp_prefix}day']
-            df_dates = metadata[METADATA_DATE_COLUMN].str.split('-', n=2, expand=True)
-            df_dates = df_dates.set_axis(temp_date_cols[:len(df_dates.columns)], axis=1)
-            missing_date_cols = set(temp_date_cols) - set(df_dates.columns)
-            for col in missing_date_cols:
-                df_dates[col] = pd.NA
+            df_dates = pd.DataFrame(
+                metadata[METADATA_DATE_COLUMN].apply(get_year_month_day).tolist(),
+                columns=temp_date_cols,
+                index=metadata.index,
+            )
             for col in temp_date_cols:
                 df_dates[col] = pd.to_numeric(df_dates[col], errors='coerce').astype(pd.Int64Dtype())
 

commit 5c73af9680745447eac34819b9a7d51d8e1813fc

Relax search pattern for 'year-month[-day]' dates
Prior to "Use standard date parsing function in subsampling" (041412c5),
this would simply split a date string on `-` to get numbers
corresponding to year/month/day. While there was no explicit
documentation of support for this format, which is a superset of the
explicitly supported ISO 8601 format, it was supported in practice.
041412c5 broke support for that format. It has been restored for
backwards compatibility in most situations (see below for caveats).

There are still some restrictions of the new relaxed search pattern that
did not exist with the former string splitting:

1. Date parts must be within valid date bounds. Prior to 041412c5, date
   parts in values such as '2010-13' were simply treated categorically
   with month=13. The values are now run through a date conversion
   function which will fail to parse out of bounds values, or in the
   case of AmbiguousDate used in <year>-<month>, auto-convert them to
   the closest in-bound value.
2. For <year>-<month>, year must be at most 4 digits, and month at most
   2 digits. Prior to 041412c5, values such as `2014-001` mapped to a
   categorical month=1. Values are now subject to maximum digits imposed
   by AmbiguousDate.regex. We could relax this regex as well, but I
   think it's better to limit rather expand support for these
   nonstandard values.

augur/dates/__init__.py
@@ -131,9 +131,26 @@ Matches:
 2. Other positive integers (e.g. 1, 123, 12345)
 """
 
-RE_YEAR_MONTH_ONLY = re.compile(r'^\d{4}-\d{2}$')
+RE_YEAR_MONTH_ONLY = re.compile(r'^\d+-\d+$')
 """
-Matches reduced precision ISO 8601 dates that are missing the day part (e.g. 2018-03).
+Matches:
+
+1. Reduced precision ISO 8601 dates that are missing the day part (e.g. 2018-03)
+2. Other dates in <year>-<month> format (e.g. 2018-3)
+
+Note: This matches out of bounds dates such as 2018-13.
+Those should be further validated by date conversion functions.
+"""
+
+RE_YEAR_MONTH_DAY = re.compile(r'^\d+-\d+-\d+$')
+"""
+Matches:
+
+1. ISO 8601 dates (e.g. 2018-03-09)
+2. Other dates in <year>-<month>-<day> format (e.g. 2018-3-9)
+
+Note: This matches out of bounds dates such as 2018-03-32.
+Those should be further validated by date conversion functions.
 """
 
 RE_AUGUR_AMBIGUOUS_DATE = re.compile(r'.*XX.*')
@@ -154,13 +171,23 @@ def get_numerical_date_from_value(value, fmt, min_max_year=None) -> Union[float,
         value = fmt.replace('%Y', value).replace('%m', 'XX').replace('%d', 'XX')
 
     if RE_YEAR_MONTH_ONLY.match(value):
-        # Note: this is already ISO 8601 so format (fmt) is ignored.
+        # Note: format (fmt) is ignored.
         start, end = AmbiguousDate(f"{value}-XX", fmt="%Y-%m-%d").range(min_max_year=min_max_year)
         return (date_to_numeric(start), date_to_numeric(end))
 
     if RE_AUGUR_AMBIGUOUS_DATE.match(value):
         start, end = AmbiguousDate(value, fmt=fmt).range(min_max_year=min_max_year)
         return (date_to_numeric(start), date_to_numeric(end))
+
+    if RE_YEAR_MONTH_DAY.match(value):
+        # Note: format (fmt) is ignored.
+        try:
+            return date_to_numeric(datetime.date(*map(int, value.split('-'))))
+        except ValueError:
+            # Note: This isn't consistent with the out of bounds handling in
+            # AmbiguousDate, which auto-converts out of bounds values to the
+            # closest in-bound value.
+            pass
 
     # Fallback: value is an exact date in the specified format (fmt).
     try: