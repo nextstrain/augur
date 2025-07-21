dd0dc73ee075b82962579b82c7db67533e7c0727
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-07-21 18:57:03 +0200

    Undo sort imports

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-import sys
-from pathlib import Path
-
+from pathlib    import Path
 import setuptools
+import sys
 
 py_min_version = (3, 9)  # minimal supported python version
 since_augur_version = (27, 0)  # py_min_version is required since this augur version
