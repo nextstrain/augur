4a1c037193a355b7e39fc8d91890dc40b658a54c
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-12-06 01:36:19 +0100

    Speed up CI (#1861)
    Speed up CI to complete within 15min (down from 25min) by switching off
    coverage except for the matrix entry that's uploaded

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# Default to running all tests.
+# Default to running all tests without coverage.
 partial_test=0
+run_coverage=0
+
+# Check if user explicitly requests coverage
+case "$@" in
+    *--cov*) run_coverage=1 ;;
+esac
 
 # If user requests a subset of tests for pytest, note this preference to avoid
 # running other tests.
@@ -9,15 +15,30 @@ case "$@" in
     *-k*) partial_test=1 ;;
 esac
 
-if [ "$partial_test" = 1 ]; then
-    # skip test coverage when running a subset of the test suite
-    coverage_arg='--no-cov'
+if [ "$run_coverage" = 1 ]; then
+    # Remove --cov from args since pytest will handle coverage via --cov=augur
+    echo "Running tests with coverage enabled, remove --cov to disable"
+    filtered_args=$(echo "$*" | sed 's/--cov//g')
+    coverage_arg='--cov=augur'
+
+    # Set env variable COVERAGE_CORE to sysmon for coverage
+    # This reduces overhead of coverage collection
+    # But only if Python version >3.14
+    if python3 -c 'import sys; print(sys.version_info >= (3, 14))' | grep -q True; then
+        echo "Using sysmon for coverage to reduce overhead"
+        export COVERAGE_CORE=sysmon
+    else
+        echo "Using default coverage collection method"
+    fi
 else
-    coverage_arg=''
+    # Default to no coverage
+    echo "Running tests without coverage, use --cov to enable"
+    filtered_args="$*"
+    coverage_arg='--no-cov'
 fi
 
 echo "Running unit tests and doctests with pytest"
-python3 -m pytest $coverage_arg "$@"
+python3 -m pytest $coverage_arg $filtered_args
 
 # Only run functional tests if we are not running a subset of tests for pytest.
 if [ "$partial_test" = 0 ]; then
