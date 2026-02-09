e417adce6d00881eb06c10b336c5fa91556b4354
james hadfield <jameshadfield@users.noreply.github.com>
2026-02-09 15:42:59 +1300

    augur proximity
    A commonly requested use-case is finding genetically-similar strains for
    a small target (query) set. We used this extensively in the early years
    of COVID, and <https://github.com/nextstrain/ncov/issues/816> explains
    the desire to shift this to a generalised augur command.
    
    This is designed to fit into our regular snakemake workflows as follows:
    * `augur filter` creates your query set and potentially also a
      contextual set if you know filters which will reduce this (e.g. clade,
      subtype, temporal range etc)
    * `augur proximity` creates a strains text file
    * `augur filter` consumes this strains file to produce the set of
      proximal sequences
    * `augur merge` combines the samples together for analysis
    
    See comments at the top of the python file for more details about the
    algorithm, as well as future directions.
    
    This uses a lot of the tricks used by
    <https://github.com/nextstrain/ncov/blob/master/scripts/get_distance_to_focal_set.py>
    howerver here our focus is on comparing queries directly to the
    contextual sequences without needing a reference sequence as a
    comparitor.
    
    An important caveat is that this approach loads all contextual sequences
    into memory (using numpy arrays, so 1 byte per character). This makes it
    unsuitable for ncov, but it should work for all our other pathogens. See
    comments in the file for ways to improve this memory bottlenech.
    
    Claude code helped with this commit.

---
+++ tests/proximity/test_utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pytest
+
+from augur.proximity import (
+    get_valid_range,
+    to_numpy_array,
+)
+
+
+class TestGetValidRange:
+    def test_start_end(self):
+        seq = to_numpy_array("NNNNNATGNN")
+        #                     0123456789
+        start,end = get_valid_range(seq)
+        assert start == 5
+        assert end == 8
