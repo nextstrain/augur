1fdf2a90ca1046e23629e1cd840811bd0f12b688
james hadfield <jameshadfield@users.noreply.github.com>
2026-03-24 12:25:43 +1300

    [ancestral] add tests to describe reconstruction bugs
    Tests describe bugs relating to our handling of ambiguous states.
    
    The nucleotide bugs are minor, relating to handling of 'X'
    
    The AA bugs are bigger, as we hardcode "N" as the ambiguous state
    however N = Asn = Asparagine.

--- tests/functional/ancestral/cram/infer-ambiguous-nucleotides.t
+++ tests/functional/ancestral/cram/infer-ambiguous-nucleotides.t
@@ -15,3 +15,166 @@ There should not be N bases in the inferred output sequences.
 
   $ grep "^N" "$CRAMTMP/$TESTFILE/ancestral_sequences.fasta"
   [1]
+
+test ambiguous bases remain in seqs and muts if we use `--keep-ambiguous`
+The ambiguous nuc 'Y' (representing a 'T' or 'C') should be reported as a mutation on the sample_C branch
+and  also remain in the resulting sequence.
+
+  $ cat > aln_pos3Y.fasta <<EOF
+  > >sample_A
+  > AAGAA
+  > >sample_B
+  > AAGAA
+  > >sample_C
+  > AAYAA
+  > EOF
+
+  $ ${AUGUR} ancestral --seed 0 \
+  >  --keep-ambiguous \
+  >  --tree $TESTDIR/../data/simple-genome/tree.nwk --alignment aln_pos3Y.fasta \
+  >  --output-node-data "$CRAMTMP/$TESTFILE/ancestral_mutations_1.json" \
+  >  --output-sequences "$CRAMTMP/$TESTFILE/ancestral_sequences_1.fasta" &> /dev/null
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_1.json" | jq -c '.nodes.sample_C.muts'
+  ["C3Y"]
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_1.json" | jq -c '.nodes.sample_C.sequence'
+  "AAYAA"
+
+  $ grep 'sample_C' -A 1 "$CRAMTMP/$TESTFILE/ancestral_sequences_1.fasta"
+  >sample_C
+  AAYAA
+
+
+Same test as above but using `--infer-ambiguous` (the default) infers Y as either T or C as appropriate
+
+  $ ${AUGUR} ancestral --seed 0 \
+  >  --infer-ambiguous \
+  >  --tree $TESTDIR/../data/simple-genome/tree.nwk --alignment aln_pos3Y.fasta \
+  >  --output-node-data "$CRAMTMP/$TESTFILE/ancestral_mutations_2.json" \
+  >  --output-sequences "$CRAMTMP/$TESTFILE/ancestral_sequences_2.fasta" &> /dev/null
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_2.json" | jq -c '.nodes.sample_C.muts'
+  []
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_2.json" | jq -c '.nodes.sample_C.sequence'
+  "AACAA"
+
+  $ grep 'sample_C' -A 1 "$CRAMTMP/$TESTFILE/ancestral_sequences_2.fasta"
+  >sample_C
+  AACAA
+
+
+Test the ambiguous nucleotide "X" is turned into a "N" with --keep-ambiguous
+'X' has a flat profile map in treetime and thus is considered ambiguous.
+Ensure it's reported as "N" regardless of whether we infer ambiguous states or not
+and reported in both sequences and mutations. Note that the parent state
+(i.e. the "G" in "G3N" below) is not the focus of this test!
+
+  $ cat > aln_pos3X.fasta <<EOF
+  > >sample_A
+  > AAGAA
+  > >sample_B
+  > AAGAA
+  > >sample_C
+  > AAXAA
+  > EOF
+
+  $ ${AUGUR} ancestral --seed 0 \
+  >  --keep-ambiguous \
+  >  --tree $TESTDIR/../data/simple-genome/tree.nwk --alignment aln_pos3X.fasta \
+  >  --output-node-data "$CRAMTMP/$TESTFILE/ancestral_mutations_3.json" \
+  >  --output-sequences "$CRAMTMP/$TESTFILE/ancestral_sequences_3.fasta" &> /dev/null
+
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_3.json" | jq -c '.nodes.sample_C.muts'
+  ["G3N"]
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_3.json" | jq -c '.nodes.sample_C.sequence'
+  "AANAA"
+
+  $ grep 'sample_C' -A 1 "$CRAMTMP/$TESTFILE/ancestral_sequences_3.fasta"
+  >sample_C
+  AANAA
+
+Test the ambiguous nucleotide "X" is turned into one of A/T/G/C with --infer-ambiguous
+Note the specific inferred state (TreeTime chooses 'G') is not the focus of the test here.
+Note 2: TreeTime chooses the root state as G as well, so there's no mutation on sample_C
+
+  $ ${AUGUR} ancestral --seed 0 \
+  >  --infer-ambiguous \
+  >  --tree $TESTDIR/../data/simple-genome/tree.nwk --alignment aln_pos3X.fasta \
+  >  --output-node-data "$CRAMTMP/$TESTFILE/ancestral_mutations_4.json" \
+  >  --output-sequences "$CRAMTMP/$TESTFILE/ancestral_sequences_4.fasta" &> /dev/null
+
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_4.json" | jq -c '.nodes.sample_C.muts'
+  []
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_4.json" | jq -c '.nodes.sample_C.sequence'
+  "AAGAA"
+
+  $ grep 'sample_C' -A 1 "$CRAMTMP/$TESTFILE/ancestral_sequences_4.fasta"
+  >sample_C
+  AAGAA
+
+
+Test the ambiguous nucleotide "X" on internal nodes is turned into an "N"  with --keep-ambiguous
+When all descendant tips of an internal node have 'X' at a position, the internal node's sequence
+should report 'N' (the standard ambiguous nucleotide) when we're not inferring ambiguous bases.
+
+  $ cat > aln_pos3X_internal.fasta <<EOF
+  > >sample_A
+  > AAXAA
+  > >sample_B
+  > AAXAA
+  > >sample_C
+  > AAXAA
+  > EOF
+
+  $ ${AUGUR} ancestral --seed 0 \
+  >  --keep-ambiguous \
+  >  --tree $TESTDIR/../data/simple-genome/tree.nwk --alignment aln_pos3X_internal.fasta \
+  >  --output-node-data "$CRAMTMP/$TESTFILE/ancestral_mutations_5.json" \
+  >  --output-sequences "$CRAMTMP/$TESTFILE/ancestral_sequences_5.fasta" &> /dev/null
+
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_5.json" | jq -c '.nodes.node_AB.sequence'
+  "AANAA"
+
+
+For a fully ambiguous column (all Ns) and a reference with 'X' (also ambiguous)
+we shouldn't report a mutation at the root!
+(Internally, the position is masked in the reconstruction, but masked root mutations are still checked.
+The reference 'X' at pos 3 should be equivalent to 'N')
+
+  $ cat > aln_col3N.fasta <<EOF
+  > >sample_A
+  > AANAA
+  > >sample_B
+  > AANAA
+  > >sample_C
+  > AANAA
+  > EOF
+
+  $ cat > ref_pos3X.fasta <<EOF
+  > >ref
+  > AAXAA
+  > EOF
+
+  $ ${AUGUR} ancestral --seed 0 \
+  >  --keep-ambiguous \
+  >  --root-sequence ref_pos3X.fasta \
+  >  --tree $TESTDIR/../data/simple-genome/tree.nwk --alignment aln_col3N.fasta \
+  >  --output-node-data "$CRAMTMP/$TESTFILE/ancestral_mutations_6.json" \
+  >  --output-sequences "$CRAMTMP/$TESTFILE/ancestral_sequences_6.fasta" &> /dev/null
+
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_6.json" | jq -c '[.nodes[].sequence] | unique'
+  ["AANAA"]
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_6.json" | jq -c '[.nodes[].muts[]]'
+  []
+
+  $ cat "$CRAMTMP/$TESTFILE/ancestral_mutations_6.json" | jq -c '.reference.nuc'
+  "AANAA"
--- tests/test_ancestral.py
+++ tests/test_ancestral.py
@@ -2,6 +2,9 @@ from augur.ancestral import run_ancestral
 from io import StringIO
 from Bio import Phylo
 import pytest
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
 
 def gather_mutations_at_pos(pos, nuc_result):
     """pos is 1-based"""
@@ -43,9 +46,6 @@ class TestRootNodeMutationAssignment:
 
     @pytest.fixture()
     def fasta_aln(self):
-        from Bio.Seq import Seq
-        from Bio.SeqRecord import SeqRecord
-        from Bio.Align import MultipleSeqAlignment
         aln = MultipleSeqAlignment(
             [
                 SeqRecord(Seq("AANATA"), id="sample_A"),
@@ -92,3 +92,127 @@ class TestRootNodeMutationAssignment:
         nuc_result = run_ancestral(T=tree, aln=vcf_aln, reference_sequence=ref, is_vcf=is_vcf, full_sequences=not is_vcf, infer_ambiguous=True, **default_augur_args)
         assert(nuc_result['root_seq'] == ref)
         assert(gather_mutations_at_pos(3, nuc_result)==[])
+
+
+class TestAmbiguousAAReconstruction:
+    # NOTE: these tests may be better expressed in `cram`, following `tests/functional/ancestral/cram/infer-ambiguous-nucleotides.t`,
+    # once `augur ancestral` can perform AA inference without needing nuc sequences.
+    # See <https://github.com/nextstrain/augur/pull/1975#discussion_r3002628926> for more discussion
+    
+    # See <https://www.bioinformatics.org/sms/iupac.html> for ambiguous bases.
+
+    @pytest.fixture
+    def tree(self):
+        t = "(sample_C:0.02,(sample_B:0.02,sample_A:0.02)node_AB:0.06)node_root:0.02;"
+        T = Phylo.read(StringIO(t), 'newick')
+        return T
+
+    @pytest.fixture
+    def generic_args(self):
+        """Generic args to run_ancestral"""
+        # Note: `augur ancestral` doesn't report full_sequences, but we want to test that here
+        args = {"reference_sequence": None, "is_vcf": False, "full_sequences": True,
+            "fill_overhangs": True, "marginal": False, "alphabet": 'aa', "rng_seed": 0}
+        return args
+
+    @pytest.mark.xfail  # TODO XXX - fix bug - we report I3N mutation, because N is hardcoded as ambiguous (nuc!) base, but N = Asn = Asparagine 
+    def test_unknown_aa_is_x(self, tree, generic_args):
+        """
+        Ensure we treat "X" as the ambiguous AA residue, not N (N = Asn = Asparagine)
+        """
+        aln = MultipleSeqAlignment([
+            SeqRecord(Seq("IIXII"), id="sample_A"),
+            SeqRecord(Seq("IIIII"), id="sample_B"),
+            SeqRecord(Seq("IIIII"), id="sample_C"),
+        ])
+        result = run_ancestral(T=tree, aln=aln, **generic_args, infer_ambiguous=False)
+        sample_a = result['mutations']['nodes']['sample_A']
+        assert sample_a['sequence'][2] == 'X'
+        pos3_muts = gather_mutations_at_pos(3, result)
+        assert len(pos3_muts)==1
+        assert pos3_muts[0] == "I3X"
+    
+    def test_unknown_aa_is_reconstructed(self, tree, generic_args):
+        """
+        Ensure "X" (the ambiguous AA residue) is reconstructed
+        """
+        aln = MultipleSeqAlignment([
+            SeqRecord(Seq("IIXII"), id="sample_A"),
+            SeqRecord(Seq("IIIII"), id="sample_B"),
+            SeqRecord(Seq("IIIII"), id="sample_C"),
+        ])
+        result = run_ancestral(T=tree, aln=aln, **generic_args, infer_ambiguous=True)
+        sample_a = result['mutations']['nodes']['sample_A']
+        assert sample_a['sequence'][2] == 'I'
+        pos3_muts = gather_mutations_at_pos(3, result)
+        assert len(pos3_muts)==0
+    
+    @pytest.mark.xfail
+    def test_invalid_residues_are_replaced_with_X(self, tree, generic_args):
+        """
+        Ensure "J" (invalid) is replaced with "X" (the ambiguous AA residue)
+        """
+        aln = MultipleSeqAlignment([
+            SeqRecord(Seq("IIJII"), id="sample_A"),
+            SeqRecord(Seq("IIIII"), id="sample_B"),
+            SeqRecord(Seq("IIIII"), id="sample_C"),
+        ])
+        result = run_ancestral(T=tree, aln=aln, **generic_args, infer_ambiguous=False)
+        sample_a = result['mutations']['nodes']['sample_A']
+        
+        # The invalid J in position 3 (idx 2) has been corrected to X
+        assert sample_a['sequence'][2] == 'X'
+        pos3_muts = gather_mutations_at_pos(3, result)
+        assert len(pos3_muts)==1
+        assert pos3_muts[0] == "I3X"
+
+    def test_ambiguous_residues_are_passed_through(self, tree, generic_args):
+        """
+        Ensure "Z" (Glx = Glutamic acid (E) or Glutamine (Q)) is passed though
+        """
+        aln = MultipleSeqAlignment([
+            SeqRecord(Seq("IIZII"), id="sample_A"),
+            SeqRecord(Seq("IIIII"), id="sample_B"),
+            SeqRecord(Seq("IIIII"), id="sample_C"),
+        ])
+        result = run_ancestral(T=tree, aln=aln, **generic_args, infer_ambiguous=False)
+        sample_a = result['mutations']['nodes']['sample_A']
+        
+        # The valid Z in position 3 (idx 2)
+        assert sample_a['sequence'][2] == 'Z'
+        pos3_muts = gather_mutations_at_pos(3, result)
+        assert len(pos3_muts)==1
+        assert pos3_muts[0].endswith("Z")
+
+    def test_invalid_residues_are_inferred(self, tree, generic_args):
+        """
+        Ensure "J" (invalid) is inferred (to I, as that's what every other residue is at this pos)
+        """
+        aln = MultipleSeqAlignment([
+            SeqRecord(Seq("IIJII"), id="sample_A"),
+            SeqRecord(Seq("IIIII"), id="sample_B"),
+            SeqRecord(Seq("IIIII"), id="sample_C"),
+        ])
+        result = run_ancestral(T=tree, aln=aln, **generic_args, infer_ambiguous=True)
+        sample_a = result['mutations']['nodes']['sample_A']
+        
+        assert sample_a['sequence'][2] == 'I'
+        pos3_muts = gather_mutations_at_pos(3, result)
+        assert len(pos3_muts)==0
+        
+    def test_ambiguous_residues_are_inferred(self, tree, generic_args):
+        """
+        Ensure "Z" (Glx = Glutamic acid (E) or Glutamine (Q)) is inferred as E or Q
+        """
+        aln = MultipleSeqAlignment([
+            SeqRecord(Seq("IIZII"), id="sample_A"),
+            SeqRecord(Seq("IIIII"), id="sample_B"),
+            SeqRecord(Seq("IIIII"), id="sample_C"),
+        ])
+        result = run_ancestral(T=tree, aln=aln, **generic_args, infer_ambiguous=True)
+        sample_a = result['mutations']['nodes']['sample_A']
+        
+        assert sample_a['sequence'][2] in ['E', 'Q']
+        pos3_muts = gather_mutations_at_pos(3, result)
+        assert len(pos3_muts)==1
+        assert pos3_muts[0] in ['I3E', 'I3Q']
