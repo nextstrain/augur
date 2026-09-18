f9e167293e6d7b975ccccbc64492fa77f146b5b9
Victor Lin <vlin@fredhutch.org>
2026-09-18 13:22:05 -0700

    export v2: Implement --config
    Adds the --config option to augur export v2 using patterns from augur
    refine's --config option.
    
    The schema generating script was updated to handle the panels option:
    
    1. "%(choices)s" placeholder expansion for description
    2. enum values for typing an array of strings or scalar string

--- augur/export_v2.py
+++ augur/export_v2.py
@@ -15,7 +15,7 @@ from inspect import cleandoc
 from typing import Dict, Union, TypedDict, Any, Tuple
 from urllib.parse import urlparse
 
-from .argparse_ import SKIP_AUTO_DEFAULT_IN_HELP, ExtendOverwriteDefault, add_validation_arguments
+from .argparse_ import SKIP_AUTO_DEFAULT_IN_HELP, ExtendOverwriteDefault, InputFile, add_validation_arguments
 from .errors import AugurError
 from .io.file import open_file
 from .io.json import MINIFY_THRESHOLD_MB, write_json
@@ -900,11 +900,12 @@ def node_data_prop_is_normal_trait(name):
 
 def register_parser(parent_subparsers):
     parser = parent_subparsers.add_parser("v2", help=__doc__)
+    parser.add_argument('--config', is_config_file_arg=True, help="config file path")
 
     required = parser.add_argument_group(
         title="REQUIRED"
     )
-    required.add_argument('--tree','-t', metavar="newick", required=True, help="Phylogenetic tree, usually output from `augur refine`")
+    required.add_argument('--tree','-t', metavar="newick", type=InputFile, required=True, help="Phylogenetic tree, usually output from `augur refine`")
     required.add_argument('--output', metavar="JSON", required=True, help="Output file (typically for visualisation in auspice)")
 
     config = parser.add_argument_group(
@@ -914,11 +915,11 @@ def register_parser(parent_subparsers):
             Supplying both is fine too, command line args will overrule what is set in the config file. \
             Multiple JSONs will be merged together, and lists present in multiple configs will be merged by extending the original list."
     )
-    config.add_argument('--auspice-config', metavar="JSON", nargs='+', action=ExtendOverwriteDefault, default=[], help="Auspice configuration file(s)")
+    config.add_argument('--auspice-config', metavar="JSON", type=InputFile, nargs='+', action=ExtendOverwriteDefault, default=[], help="Auspice configuration file(s)")
     config.add_argument('--title', type=str, metavar="title", help="Title to be displayed by auspice")
     config.add_argument('--maintainers', metavar="name", action=ExtendOverwriteDefault, nargs='+', help="Analysis maintained by, in format 'Name <URL>' 'Name2 <URL>', ...")
     config.add_argument('--build-url', type=str, metavar="url", help="Build URL/repository to be displayed by Auspice")
-    config.add_argument('--description', metavar="description.md", help="Markdown file with description of build and/or acknowledgements to be displayed by Auspice")
+    config.add_argument('--description', metavar="description.md", type=InputFile, help="Markdown file with description of build and/or acknowledgements to be displayed by Auspice")
     config.add_argument('--warning', metavar="text or file", help="Text or file in Markdown format to be displayed as a warning banner by Auspice")
     config.add_argument('--geo-resolutions', metavar="trait", nargs='+', action=ExtendOverwriteDefault, help="Geographic traits to be displayed on map")
     config.add_argument('--color-by-metadata', metavar="trait", nargs='+', action=ExtendOverwriteDefault,
@@ -933,11 +934,11 @@ def register_parser(parent_subparsers):
     optional_inputs = parser.add_argument_group(
         title="OPTIONAL INPUT FILES"
     )
-    optional_inputs.add_argument('--node-data', metavar="JSON", nargs='+', action=ExtendOverwriteDefault,
+    optional_inputs.add_argument('--node-data', metavar="JSON", type=InputFile, nargs='+', action=ExtendOverwriteDefault,
         help="JSON files containing metadata for nodes in the tree. " +
              "Keys are automatically exported as colorings unless special-cased. " +
              "URLs for a key 'X' can be stored under key 'X__url' and will be automatically exported.")
-    optional_inputs.add_argument('--metadata', metavar="FILE",
+    optional_inputs.add_argument('--metadata', metavar="FILE", type=InputFile,
         help="Additional metadata for strains in the tree. " +
              "Columns are not typically exported by default and must be specified via arguments or within the config JSON. "
              "URLs for a column 'X' can be stored in column 'X__url' and will be automatically exported.")
@@ -945,8 +946,8 @@ def register_parser(parent_subparsers):
                                  help="delimiters to accept when reading a metadata file. Only one delimiter will be inferred.")
     optional_inputs.add_argument('--metadata-id-columns', default=DEFAULT_ID_COLUMNS, nargs="+", action=ExtendOverwriteDefault,
                                  help="names of possible metadata columns containing identifier information, ordered by priority. Only one ID column will be inferred.")
-    optional_inputs.add_argument('--colors', metavar="FILE", help="Custom color definitions, one per line in the format `TRAIT_TYPE\\tTRAIT_VALUE\\tHEX_CODE`")
-    optional_inputs.add_argument('--lat-longs', metavar="TSV",
+    optional_inputs.add_argument('--colors', metavar="FILE", type=InputFile, help="Custom color definitions, one per line in the format `TRAIT_TYPE\\tTRAIT_VALUE\\tHEX_CODE`")
+    optional_inputs.add_argument('--lat-longs', metavar="TSV", type=InputFile,
         help=cleandoc(f"""
             Latitudes and longitudes for geography traits. See this file for the format:
             <https://github.com/nextstrain/augur/blob/{__version__}/augur/data/lat_longs.tsv>.
--- docs/usage/cli/export.rst
+++ docs/usage/cli/export.rst
@@ -4,9 +4,24 @@ augur export
 
 .. note:: The behavior of ``augur export`` has changed in v6. Please see :doc:`here <../../releases/migrating-v5-v6>` for more details.
 
+.. contents:: Table of Contents
+   :local:
+   :depth: 1
+
+Command-line arguments
+======================
+
 .. argparse::
     :module: augur
     :func: make_parser
     :prog: augur
     :path: export
+
+Configuration for v2
+====================
+
+Options can also be specified in a YAML configuration file supplied to
+``--config`` with the following top-level keys:
+
+.. schema-options-table:: augur/data/schema-export-v2-config.json
         
